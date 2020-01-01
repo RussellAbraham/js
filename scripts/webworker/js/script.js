4036e028baeab0525e75838e17b0e4ce0db870c0
Russell <35252806+RussellAbraham@users.noreply.github.com>
2019-12-31 23:07:44 -0800

    Update script.js

--- scripts/webworker/js/script.js
+++ scripts/webworker/js/script.js
@@ -1,5 +1,3 @@
-// small script to add random messages like advertisements to the page
-
 const webworker = new Worker("/js/worker.js");
 
 const annoyer = {
@@ -9,9 +7,7 @@ const annoyer = {
   ],
 
   pickPhrase() {
-    const {
-      phrases
-    } = this;
+    const { phrases } = this;
     const idx = Math.floor(Math.random() * phrases.length);
     return phrases[idx]
   },
@@ -19,28 +15,26 @@ const annoyer = {
   start() {
     //Use an arrow function to avoid getting a different 'this':
     this.timerId = setInterval(() => {
-
-      console.log(this.pickPhrase(), this)
-      webworker.postMessage(this.pickPhrase());
-
-    }, 10000)  
+      let msg = this.pickPhrase();
+      console.log(msg, this)
+      webworker.postMessage(msg);
+      setText(target, msg);
+    }, 1000)  
   },
   
   stop() {
     clearInterval(this.timerId);
     console.log("PHEW THANK HEAVENS THAT IS OVER!")  
   }
-
   
 }
 
-annoyer.start();
+// annoyer.start();
 // annoyer.stop();
-
 
 webworker.onmessage = function(e){
   console.log(
     "I Document, Received Message From " + this + "\n" +
     "Message: " + e.data
   );
-}
+};
