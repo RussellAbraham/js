2188c9ff6356654f758131b709523922dfef4f15
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-01-06 23:47:25 -0800

    Update script.js

--- scripts/webworker/script.js
+++ scripts/webworker/script.js
@@ -1,45 +1,61 @@
-
-// "/" = "https://russellabraham.github.io/"
-// "./" = "https://russellabraham.github.io/javascript/webworker/"
-
-const webworker = new Worker("./worker.js");
+const webworker = new Worker('worker.js');
 
 const annoyer = {
-  
-  phrases: [  
-    "Updating..", "Adding..", "Deleting..", "Testing..", "Searching..", "Please Wait.."  
-  ],
-
-  pickPhrase() {
-    const { phrases } = this;
-    const idx = Math.floor(Math.random() * phrases.length);
-    return phrases[idx]
-  },
-  
-  start() {
-    //Use an arrow function to avoid getting a different 'this':
-    this.timerId = setInterval(() => {
-      let msg = this.pickPhrase();
-      console.log(msg, this)
-      webworker.postMessage(msg);
-      setText(target, msg);
-    }, 1000)  
-  },
-  
-  stop() {
-    clearInterval(this.timerId);
-    console.log("PHEW THANK HEAVENS THAT IS OVER!")  
+    phrases: [  
+      "Hello World!", "Random Text", "Goodbye World!", "Testing Text", "More Text"  
+    ],
+    pickPhrase() {
+      const { phrases } = this;
+      const idx = Math.floor(Math.random() * phrases.length);
+      return phrases[idx]
+    },  
+    start() {
+      this.timerId = setInterval(() => {
+        webworker.postMessage(this.pickPhrase());
+      }, 1000)  
+    },  
+    stop() {
+      clearInterval(this.timerId);
+      console.log("PHEW THANK HEAVENS THAT IS OVER!")  
+    }
   }
-  
-}
 
-// annoyer.start();
-// annoyer.stop();
+  webworker.onmessage = function(e){
+    if(isObject(e.data)){
+      console.log('its obj');
+      console.log(e.data);
+    } else if(isString(e.data)) {
+      var obj = JSON.parse(e.data);
+      console.info('was str now obj');
+      console.log(obj);
+      populate(obj)
+    } else {
+      console.error('its not what you thot')
+    }
+    annoyer.stop()
+  };
 
-webworker.onmessage = function(e){
-  console.log(
-    "I Document, Received Message From " + this + "\n" +
-    "Message: " + e.data
-  );
-  setHTML(outputbody, "I Document, Received Message: " + "<code>" + e.data + "</code>");
-};
+  var arr = [];
+  function populate(obj){
+    console.info('ok DOM got obj');  
+    //console.log(obj);
+    arr.push(_.values(obj))
+    console.log(_.values(obj));
+    //console.log(_.values(obj)[0]);
+   
+  }
+
+
+//
+//  var x = arr[0][2]
+//
+//  var y = x[0].children;
+//
+//  console.log(y[0].path)
+
+
+
+// var x = arr[0][2];
+// var css = x[0].children;
+// var js  = x[2].children;
+// var svg = x[10].children;
