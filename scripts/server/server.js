4f365cadcb9ae96a57ee5accbe4104ead1e6635d
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-01-14 21:47:32 -0800

    Update server.js

--- scripts/server/server.js
+++ scripts/server/server.js
@@ -10,6 +10,13 @@ http.createServer(function (request, response) {
     response.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     response.setHeader('Access-Control-Allow-Credentials', true);
     
+    // response.setHeader({
+    //     'Access-Control-Allow-Origin': '*',
+    //     'Access-Control-Allow-Methods': [ 'GET, POST, OPTIONS, PUT, PATCH, DELETE' ],
+    //     'Access-Control-Allow-Headers': 'X-Requested-With,content-type',
+    //     'Access-Control-Allow-Credentials': true
+    // });
+    
     var filePath = '.' + request.url;
     if (filePath == './') {
         filePath = './index.html';
