09486e20edca49d06ca6755b37db6224f67b40ba
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-01-17 00:41:30 -0800

    Create query.js

---
+++ scripts/query.js
@@ -0,0 +1,73 @@
+function addhttp(url) {
+  if (!/^(?:f|ht)tps?\:\/\//.test(url)) {
+    url = "http://" + url;
+  }
+  return url;
+}
+
+var parseQuery = function parseQuery() {
+  var search = window.location.search;
+  var q = {};
+  search
+    .slice(1)
+    .split("&")
+    .forEach(function(term) {
+      var kv = term.split("=");
+      q[kv[0]] = kv[1] !== undefined ? decodeURIComponent(kv[1]) : true;
+    });
+  return q;
+};
+
+function getQueryParams(qs) {
+  qs = qs.split("+").join(" ");
+  var params = {},
+    tokens,
+    re = /[?&]?([^=]+)=([^&]*)/g;
+  while ((tokens = re.exec(qs))) {
+    params[decodeURIComponent(tokens[1])] = decodeURIComponent(tokens[2]);
+  }
+  return params;
+}
+
+function getUrlParameter(name) {
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+    var results = regex.exec(location.search);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+};
+
+function getAllUrlParams(url) {
+  var queryString = url ? url.split("?")[1] : window.location.search.slice(1);
+  var obj = {};
+  if (queryString) {
+    queryString = queryString.split("#")[0];
+    var arr = queryString.split("&");
+    for (var i = 0; i < arr.length; i++) {
+      var a = arr[i].split("=");
+      var paramName = a[0];
+      var paramValue = typeof a[1] === "undefined" ? true : a[1];
+      paramName = paramName.toLowerCase();
+      if (typeof paramValue === "string") paramValue = paramValue.toLowerCase();
+      if (paramName.match(/\[(\d+)?\]$/)) {
+        var key = paramName.replace(/\[(\d+)?\]/, "");
+        if (!obj[key]) obj[key] = [];
+        if (paramName.match(/\[\d+\]$/)) {
+          var index = /\[(\d+)\]/.exec(paramName)[1];
+          obj[key][index] = paramValue;
+        } else {
+          obj[key].push(paramValue);
+        }
+      } else {
+        if (!obj[paramName]) {
+          obj[paramName] = paramValue;
+        } else if (obj[paramName] && typeof obj[paramName] === "string") {
+          obj[paramName] = [obj[paramName]];
+          obj[paramName].push(paramValue);
+        } else {
+          obj[paramName].push(paramValue);
+        }
+      }
+    }
+  }
+  return obj;
+}
