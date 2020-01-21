66dff00ae84fb10500fa77bd221d58c61c55be68
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-01-20 18:03:53 -0800

    Update script.js

--- scripts/client/mutation-observer/indexeddb/script.js
+++ scripts/client/mutation-observer/indexeddb/script.js
@@ -18,17 +18,13 @@ $("form").on("submit", function(event){
 });
 
 
-
-
 /*   */
-
 function S4() {
    return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
 };
 function guid() {
    return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
 };
-
 
 
 localforage.setDriver(localforage.INDEXEDDB);
