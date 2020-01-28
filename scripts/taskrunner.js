93d2c48f25f0a5162e7dc295cfde29071ef2a02c
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-01-28 08:18:33 -0800

    Update taskrunner.js

--- scripts/taskrunner.js
+++ scripts/taskrunner.js
@@ -1,3 +1,4 @@
+
 const taskrunner = {
     start:function(){
         this.timerId = setInterval(function(){
@@ -9,3 +10,30 @@ const taskrunner = {
         console.log('taskrunner may have stopped')
     }
 }
+
+// taskrunner.start();
+// taskrunner.stop();
+
+
+// factory function 
+const makeRunner = () => {
+    return {
+        start: function(){
+            this.timeId = setInterval(function(){
+                //
+            }, 1000)
+        },
+        stop: function(){
+            clearInterval(this.timerId);
+            console.log('stopped');
+        }
+    }
+}
+
+// const r1 = makeRunner();
+// r1.start();
+// r1.stop();
+// 
+// const r2 = makeRunner();
+// r2.start();
+// r2.stop();
