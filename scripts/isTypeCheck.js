3891ebe0ec6e6220d88de1c9769853a65c75168a
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-01-29 13:06:17 -0800

    Update and rename isType.js to isTypeCheck.js

--- scripts/isTypeCheck.js
+++ scripts/isTypeCheck.js
@@ -148,3 +148,44 @@ function isLocation(obj){
 // [native code] 
 
 
+function check(arg){
+    if(isBoolean(arg)){
+        console.log('boolean', arg)
+    }
+    if(isNull(arg)){
+        console.log('null', arg)
+    }
+    if(isNan(arg)){
+        console.log('NaN', arg)
+    }
+    if(isUndefined(arg)){
+        console.log('undefined', arg)
+    }
+    if(isNumber(arg)){
+        console.log('number', arg)
+    }
+    if(isPrime(arg)){
+        console.log('prime', arg)
+    }
+    if(isEven(arg)){
+        console.log('even', arg)
+    }    
+    if(isString(arg)){
+        console.log('string', arg)
+    }
+    if(isSymbol(arg)){
+        console.log('symbol', arg)
+    }
+    if(isObject(arg)){
+        console.log('object', arg)
+    }
+    if(isArray(arg)){
+        console.log('array', arg)
+    }
+    if(isElement(arg)){
+        console.log('element', arg)
+    }
+    if(isLocation(arg)){
+        console.log('location', arg)
+    }
+}
