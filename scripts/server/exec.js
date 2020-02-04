9ae344f317ed544c52bcb38cd4abc2e46dad09df
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-02-04 00:38:04 -0800

    Create exec.js

---
+++ scripts/server/exec.js
@@ -0,0 +1,10 @@
+const { exec } = require('child_process');
+
+// execute batch file
+exec('my.bat', (err, stdout, stderr) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(stdout);
+});
