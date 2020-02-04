1fa5223ba22018d7926020aa9e6fc7e4ad22b5da
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-02-04 00:35:31 -0800

    Update element.js

--- scripts/element.js
+++ scripts/element.js
@@ -1,7 +1,33 @@
-function element(target, element, attrs){
+function element0(target, element, attrs){
     const myElement = document.createElement(element);
     for(var attr in attrs){
       myElement.setAttribute(attr, attrs[attr])
     }
     return target.appendChild(myElement);
 }
+element0(document.body, 'textarea', {
+	placeholder: 'textarea'
+});
+
+function element(target, element, attrs, text){	
+	const myElement = document.createElement(element);
+	const myTextNode = document.createTextNode(text);
+	for(var attr in attrs){
+		myElement.setAttribute(attr, attrs[attr])
+	}
+  if(text){
+		myElement.appendChild(myTextNode);
+	}
+	return target.appendChild(myElement);
+}
+element(
+	document.body, 
+	'h1', 
+	{ 
+		id:'container',
+		class: 'container',
+		style: 'font-family:monospace;',
+		onclick:'fn()'
+	}, 
+	'Hello World'
+);
