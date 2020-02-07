d5f929fb5a348a293c070c925e776f6699eab56e
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-02-07 01:51:36 -0800

    Update taskrunner.js

--- scripts/taskrunner.js
+++ scripts/taskrunner.js
@@ -1,9 +1,14 @@
+// continues running a function every tick of the timer
+
 const taskrunner = {
   start: function () {
+    // if the timerId exists, dont start another one
     if (this.timerId) {
       console.log('already running')
       return false
-    } else {
+    } 
+    // if timerId does not exist, start the interval
+    else {
       this.timerId = setInterval(function () {
         // execute function
       }, 1000);
@@ -12,6 +17,7 @@ const taskrunner = {
   stop: function () {
     clearInterval(this.timerId);
     console.log('taskrunner may have stopped');
+    // clear the timerId so it can start again
     this.timerId = "";
   }
 }
