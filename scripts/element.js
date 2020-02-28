8ddce86b2473829c3ea1e8f7444913e3c4cddbed
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-02-27 19:26:33 -0800

    Update element.js

--- scripts/element.js
+++ scripts/element.js
@@ -1,33 +1,12 @@
-function element0(target, element, attrs){
-    const myElement = document.createElement(element);
-    for(var attr in attrs){
-      myElement.setAttribute(attr, attrs[attr])
-    }
-    return target.appendChild(myElement);
-}
-element0(document.body, 'textarea', {
-	placeholder: 'textarea'
-});
-
+/* var domNodeParent, domNodeChild, domNodeFragment = new DocumentFragment(); */
 function element(target, element, attrs, text){	
 	const myElement = document.createElement(element);
 	const myTextNode = document.createTextNode(text);
 	for(var attr in attrs){
 		myElement.setAttribute(attr, attrs[attr])
 	}
-  if(text){
+  	if(text){
 		myElement.appendChild(myTextNode);
 	}
 	return target.appendChild(myElement);
 }
-element(
-	document.body, 
-	'h1', 
-	{ 
-		id:'container',
-		class: 'container',
-		style: 'font-family:monospace;',
-		onclick:'fn()'
-	}, 
-	'Hello World'
-);
