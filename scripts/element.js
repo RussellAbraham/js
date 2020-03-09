58fe1e5d5bc40a3b458964fb2c98c785f0331b66
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-03-08 17:33:49 -0700

    Update element.js

--- scripts/element.js
+++ scripts/element.js
@@ -1,4 +1,5 @@
 /* var domNodeParent, domNodeChild, domNodeFragment = new DocumentFragment(); */
+
 function element(target, element, attrs, text){	
 	const myElement = document.createElement(element);
 	const myTextNode = document.createTextNode(text);
@@ -10,3 +11,12 @@ function element(target, element, attrs, text){
 	}
 	return target.appendChild(myElement);
 }
+
+function insertTextNodeBefore(className, text) {
+  var newItem = document.createElement("pre");
+  var textnode = document.createTextNode(text);
+  newItem.className = "list-group-item " + className;
+  newItem.appendChild(textnode);
+  var list = document.getElementById("output");
+  list.insertBefore(newItem, list.childNodes[0]);
+} 
