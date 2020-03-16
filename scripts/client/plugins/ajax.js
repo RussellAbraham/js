5cc55c3f926990a384085860e1fc7a6f106151df
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-03-15 17:36:06 -0700

    Update ajax.js

--- scripts/client/plugins/ajax.js
+++ scripts/client/plugins/ajax.js
@@ -1,13 +1,13 @@
 function $GET(url){
   $.ajax({
-	  url : url,
-	  type : "GET",
-	  data : { "key" : "value" },
-	  content : "application/json; charset=utf-8",
-    async : true,
-	  dataType : 'json',
-	  success : function(data, status, xhr) {		
-	    console.log(data, status, xhr);
+	url : url,
+	type : "GET",
+	data : { "key" : "value" },
+	content : "application/json; charset=utf-8",
+    	async : true,
+	dataType : 'json',
+	success : function(data, status, xhr) {		
+	console.log(data, status, xhr);
     }
   });
 }
