e566cd40a334136f3804a8483178be2e8f0e4dd6
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-03-21 01:14:23 -0700

    Update listen.js

--- scripts/listen.js
+++ scripts/listen.js
@@ -4,28 +4,28 @@ function on(element, event, callback){
   return element.addEventListener(event, callback);
 }
 
-/* Example
-on(docuement.getElementById('myId'), 'input', function(event){
-  console.log(event.target);
-}); 
-*/
-
 function listen(element, events){
   for(var event in events){
     element.addEventListener(event, events[event]);
   }
 }
 
+
 /* Example
+
+on(docuement.getElementById('myId'), 'input', function(event){
+  console.log(event.target);
+}); 
+
 listen(this, {
   'click' : function(){},
   'mouseover' : function(){},
   'mousemove' : handleUpdate,
   'mouseout' : function(){},
   'touchstart' : function(),
-	'touchend' : function(),
-	'touchcancel' : function(),
-	'touchmove' : function(),
+  'touchend' : function(),
+  'touchcancel' : function(),
+  'touchmove' : function(),
   'dragstart' : function(){},
   'dragend' : function(){},
   'dragover' : function(){},
