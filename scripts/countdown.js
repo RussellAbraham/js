38b3a1a2e2a4f11c313b0d20f17ccf0e92262e8d
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-04-04 20:43:54 -0700

    Update countdown.js

--- scripts/countdown.js
+++ scripts/countdown.js
@@ -1,3 +1,16 @@
+function loader(secs,elem) {
+    var element = document.getElementById(elem);
+    element.innerHTML = "Please wait for "+secs+" seconds";
+    if(secs < 1) {
+        clearTimeout(timer);
+        element.innerHTML = '<h2>Countdown Complete!</h2>';
+        element.innerHTML += '<a href="#">Click here now</a>';
+    }
+    secs--;
+    var timer = setTimeout('countDown('+secs+',"'+elem+'")',1000);
+}
+
+
 function countDown(num){
     if(num <= 0) {
         console.log("All done!");
