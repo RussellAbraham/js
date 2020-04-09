a12f1ff378acd1f336d1908699d0a1ce2db998c7
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-04-09 04:54:01 -0700

    Update object.js

--- object.js
+++ object.js
@@ -9,7 +9,30 @@
     object['function'] = function(obj){
         return Function('"use strict";return (' + obj + ')')();
     }
-
+    
+    function listen(object, events){
+		for(var event in events){
+			object.addEventListener(event, events[event])
+		}
+	}
+	function Template(links){
+        var self = this;
+		self.links = links;
+        self.index = 0;
+		var i, len = self.links.length;
+		for(i = 0;i < len;i++){
+			listen(window['links' + i], {
+				'click' : function(){}.bind(object, i),
+				'touchstart' : function(){}.bind(object, i),
+				'touchend' : function(){}.bind(object, i)
+			})
+		}	
+	}
+    
+	object['template'] = function(obj){
+		return Template(obj);
+	}
+    
     if(typeof window !== 'undefined'){
         window.object = object;
     }
