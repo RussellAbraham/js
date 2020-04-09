9334b7c9953160cdc0ac2dd5a85b9e61334b3801
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-04-09 05:15:10 -0700

    Update object.js

--- object.js
+++ object.js
@@ -1,47 +1,75 @@
-(function(object){
-    
-	['Arguments', 'Function', 'String', 'Number', 'Date', 'RegExp', 'Error'].forEach(function(name){        
-		object['is' + name] = function(obj){            
-			return toString.call(obj) === '[object ' + name + ']';        
-		}    
-	});
-    
-	object['function'] = function(obj){        
-		return Function('"use strict";return (' + obj + ')')();   
-	}
-        
-	function listen(object, events){			
-		for(var event in events){
-			object.addEventListener(event, events[event])
-		}	    
-	}
-	
-	function Template(links){        
-		var self = this;
-		self.links = links;       
-		self.index = 0;		
-		var i, len = self.links.length;		
-		for(i = 0;i < len;i++){		
-			listen(window['links' + i], {			
-				'click' : function(){
-					// 				
-				}.bind(object, i),				
-				'touchstart' : function(){				
-					//
-				}.bind(object, i),				
-				'touchend' : function(){
-					//				
-				}.bind(object, i)			
-			})		
-		}		
-	}
-    	
-	object['template'] = function(obj){	
-		return Template(obj);	
-	}
-        
-	if(typeof window !== 'undefined'){        
-		window.object = object;    
-	}
+(function (object) {
+    var property = function(key) {
+        return function(obj) {
+          return obj == null ? void 0 : obj[key];
+        };  
+    };	
+    var MAX_ARRAY_INDEX = Math.pow(2, 53) - 1;
+    var getLength = property('length');
+    /* *** isArray()    *** */
+    function isArray(obj) {
+        return toString.call(obj) === "[object Array]";
+    }
+    var isArrayLike = function(collection) {
+        var length = getLength(collection);
+        return typeof length == 'number' && length >= 0 && length <= MAX_ARRAY_INDEX;
+    };
+    /* *** isBuffer() *** */
+    function isBuffer(val) {
+        return val !== null && !isUndefined(val) && val.constructor !== null && !isUndefined(val.constructor) &&
+            typeof val.constructor.isBuffer === 'function' && val.constructor.isBuffer(val);
+    }
+    /* *** isArrayBuffer *** */
+    function isArrayBufferView(val) {
+        var result;
+        if ((typeof ArrayBuffer !== 'undefined') && (ArrayBuffer.isView)) {
+            result = ArrayBuffer.isView(val);
+        } else {
+            result = (val) && (val.buffer) && (val.buffer instanceof ArrayBuffer);
+        }
+        return result;
+    }
+    /* *** isObject()    *** */
+    function isObject(obj) { 
+	    return obj === Object(obj); 
+    }
+    /* *** isObjectLike()    *** */
+    function isObjectLike(value) { 
+	    return value != null && typeof value == 'object'; 
+    }
+
+    object['isArray'] = function(obj){
+        return isArray(obj)
+    }
+    object['isArrayLike'] = function(obj){
+        return isArrayLike(obj)
+    }
+    object['isBuffer'] = function(obj){
+        return isBuffer(obj)
+    }
+    object['isArrayBufferView'] = function(obj){
+        return isArrayBufferView(obj)
+    }	
+    object['isObject'] = function(obj){
+        return isObject(obj)
+    }
+    object['isObjectLike'] = function(obj){
+        return isObjectLike(obj)
+    }
+
+    ['Arguments', 'Location', 'Function', 'String', 'Number', 'Date', 'RegExp', 'Error'].forEach(function (name) {
+        object['is' + name] = function (obj) {
+            return toString.call(obj) === '[object ' + name + ']';
+        }
+    });
+
+
+    object['function'] = function (obj) {
+        return Function('"use strict";return (' + obj + ')')();
+    }
+
+    if (typeof window !== 'undefined') {
+        window.object = object;
+    }
 
 })(new Object())
