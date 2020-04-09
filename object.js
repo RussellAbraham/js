4a4fd8631a487fbbcf85defa0436cc81aaa8a021
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-04-09 04:57:26 -0700

    Update object.js

--- object.js
+++ object.js
@@ -1,40 +1,47 @@
 (function(object){
-
-    ['Arguments', 'Function', 'String', 'Number', 'Date', 'RegExp', 'Error'].forEach(function(name){
-        object['is' + name] = function(obj){
-            return toString.call(obj) === '[object ' + name + ']';
-        }
-    });
-
-    object['function'] = function(obj){
-        return Function('"use strict";return (' + obj + ')')();
-    }
     
-    function listen(object, events){
+	['Arguments', 'Function', 'String', 'Number', 'Date', 'RegExp', 'Error'].forEach(function(name){        
+		object['is' + name] = function(obj){            
+			return toString.call(obj) === '[object ' + name + ']';        
+		}    
+	});
+    
+	object['function'] = function(obj){        
+		return Function('"use strict";return (' + obj + ')')();   
+	}
+        
+	function listen(object, events){			
 		for(var event in events){
 			object.addEventListener(event, events[event])
-		}
+		}	    
 	}
-	function Template(links){
-        var self = this;
-		self.links = links;
-        self.index = 0;
-		var i, len = self.links.length;
-		for(i = 0;i < len;i++){
-			listen(window['links' + i], {
-				'click' : function(){}.bind(object, i),
-				'touchstart' : function(){}.bind(object, i),
-				'touchend' : function(){}.bind(object, i)
-			})
-		}	
+	
+	function Template(links){        
+		var self = this;
+		self.links = links;       
+		self.index = 0;		
+		var i, len = self.links.length;		
+		for(i = 0;i < len;i++){		
+			listen(window['links' + i], {			
+				'click' : function(){
+					// 				
+				}.bind(object, i),				
+				'touchstart' : function(){				
+					//
+				}.bind(object, i),				
+				'touchend' : function(){
+					//				
+				}.bind(object, i)			
+			})		
+		}		
 	}
-    
-	object['template'] = function(obj){
-		return Template(obj);
+    	
+	object['template'] = function(obj){	
+		return Template(obj);	
 	}
-    
-    if(typeof window !== 'undefined'){
-        window.object = object;
-    }
+        
+	if(typeof window !== 'undefined'){        
+		window.object = object;    
+	}
 
 })(new Object())
