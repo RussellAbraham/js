d01c6ff482f04fe1e8741a75f92fb467d3246642
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-04-26 21:07:20 -0700

    Update count.js

--- scripts/count.js
+++ scripts/count.js
@@ -1,18 +1,49 @@
-var count = 0;
-var full = false;
-var max = 256;
+(function(root){
 
-function reset(){
-  count = 0;
-  full = false;
-}
+	var count = 0;
+	var full = false;
+	var max = 256;
 
-function add(){
-	if(!full){
-		count++;
-		if(count === max){
-			full = true;
-		}
-		console.log(count);
+	function reset(){
+	  countDown(count)	
+	  // count = 0;
+	  // full = false;
 	}
-}
+
+	 function countUp(){
+		if(!full){
+			count++;
+			if(count === max){
+				full = true;
+			}
+			console.log(count);
+		}
+	}
+
+	function countDown(num){
+	  if (num <= 0) {
+	    console.log("Complete", count);
+	    return;
+	  }
+	  num--;
+	  count--;
+	  setTimeout(function(num){
+	  num = num || 0;      
+	      countDown(num);
+	      console.log(num--);
+	  }, 250, num);
+	
+	}
+	
+	const api = {
+		reset : reset,
+		countUp : countUp,
+		countDown : countDown
+	}
+	
+	if(typeof root !== 'undefined'){
+		root.api = api;
+	}
+	
+
+})(this);
