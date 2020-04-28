cca639fa5266905d2135d1f55e40fe5ea980659f
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-04-27 23:53:03 -0700

    Create prefix.js

---
+++ scripts/prefix.js
@@ -0,0 +1,12 @@
+var prefix = (function prefix() {
+  var regex = /^(Webkit|Khtml|Moz|ms|O)(?=[A-Z])/;
+  var styleDeclaration = doc.getElementsByTagName('script')[0].style;
+  for (var prop in styleDeclaration) {
+    if (regex.test(prop)) {
+      return '-' + prop.match(regex)[0].toLowerCase() + '-';
+    }
+  }
+  if ('WebkitOpacity' in styleDeclaration) { return '-webkit-'; }
+  if ('KhtmlOpacity' in styleDeclaration) { return '-khtml-'; }
+  return '';
+}());
