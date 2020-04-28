699963555d195cce5475e25e6be69d7af056b594
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-04-28 04:59:08 -0700

    Update extend.js

--- scripts/extend.js
+++ scripts/extend.js
@@ -1,3 +1,14 @@
+//
+function extend(obj){
+	Array.prototype.slice.call(arguments, 1).forEach(function(source){
+      		for (var prop in source) {
+	        		if (source[prop] !== void 0) obj[prop] = source[prop];
+      		}
+	});
+	return obj;
+}
+
+//
 window.extend = function (obj, extObj) {
     obj = obj || {};
     if (arguments.length > 2) {
