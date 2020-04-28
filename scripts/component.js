05fe379cf7301814aa3ecf614a7d6e9d296669b4
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-04-27 23:52:47 -0700

    Update component.js

--- scripts/component.js
+++ scripts/component.js
@@ -1,6 +1,38 @@
+
+// support for touch events
+var msPointerSupported = window.navigator.msPointerEnabled;
+var touch = {
+  'start': msPointerSupported ? 'MSPointerDown' : 'touchstart',
+  'move': msPointerSupported ? 'MSPointerMove' : 'touchmove',
+  'end': msPointerSupported ? 'MSPointerUp' : 'touchend'
+};
+// prefix support
+var prefix = (function prefix() {
+  var regex = /^(Webkit|Khtml|Moz|ms|O)(?=[A-Z])/;
+  var styleDeclaration = doc.getElementsByTagName('script')[0].style;
+  for (var prop in styleDeclaration) {
+    if (regex.test(prop)) {
+      return '-' + prop.match(regex)[0].toLowerCase() + '-';
+    }
+  }
+  if ('WebkitOpacity' in styleDeclaration) { return '-webkit-'; }
+  if ('KhtmlOpacity' in styleDeclaration) { return '-khtml-'; }
+  return '';
+}());
+
+
 function Component(options) {
     options = options || {};
+    
+    this._start = 0;
+    this._current = 0;
     this._opened = false;
+    
+    this.main = options.main;
+    this.aside = options.aside;
+    
+    
+    
 }
 
 Component.prototype = {
