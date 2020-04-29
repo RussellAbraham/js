43cc06a63f00dfa5b1d3b988cc14e1102b286e76
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-04-28 21:23:15 -0700

    Update render.js

--- scripts/render.js
+++ scripts/render.js
@@ -1,42 +1,13 @@
-function pageLog(node, sMsg){ 
-  var oFragm = document.createDocumentFragment(); 
-  oFragm.appendChild(document.createTextNode(sMsg)); 
-  oFragm.appendChild(document.createElement('br')); 
-  document.querySelector(node).appendChild(oFragm); 
-} 
-
-function appendContent(node, content) {
-  var ele = document.querySelector(node)
-  if (ele) {
-    ele.insertAdjacentHTML("beforeend", content);
-  }
-}
-
-function prependContent(node, content) {
-  var ele = document.querySelector(node)
-  if (ele) {
-    ele.insertAdjacentHTML("afterbegin", content);
-  }
-}
-
-function insertBefore(node, content){
-  var ele = document.querySelector(node)
-  ele.insertBefore(content, ele.childNodes[0]);
-}
-
-function setContent(node, content) {
-  var ele = querySelector(node);
-  if (ele) { 
-    ele.innerHTML = content;
-  }
-}
-
-// 
 var ele;
 
 const selector = document.querySelector.bind(document);
-const b = 'beforeend';
-const a = 'afterbegin';
+
+function pageLog(node, sMsg){ 
+  var oFragm = document.createDocumentFragment();
+  oFragm.appendChild(document.createTextNode(sMsg)); 
+  oFragm.appendChild(document.createElement('br')); 
+  selector(node).appendChild(oFragm); 
+} 
 
 function appendHtml(node, content){
     ele = selector(node);
@@ -53,13 +24,14 @@ function innerHtml(node, content){
     ele.innerHTML = content;
 }
 
-function adjacentHtml(node, x, content){
+function adjacentHtml(node, side, content){
     ele = selector(node);
     if (ele) {
-        switch(x){
+        switch(side){
             case 'b' : ele.insertAdjacentHTML('beforeend', content); break;
             case 'a' : ele.insertAdjacentHTML('afterbegin', content); break;
         }
     }
 }
+
 
