77d47d6ab684be8f0205c29d9366c0213fef9833
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-06-24 16:10:47 -0700

    Update header.js

--- backbone/scripts/templates/header.js
+++ backbone/scripts/templates/header.js
@@ -1,25 +1,23 @@
 var HeaderTemplate = _.template(
+	'<header class="navbar" id="header">'+	    
 	
-  '<header class="navbar" id="header">'+
-	
-    '<nav class="btn-group">'+	
-			'<% _.each(arr, function(obj, index, arr) { %>' +
-	    	'<a class="<%= obj.class %>"><%= obj.value %></a>' +
-	    '<% }); %>' +  
+		'<nav class="btn-group">'+				
+			'<% _.each(arr, function(obj, index, arr) { %>' +	
+				'<a class="<%= obj.class %>"><%= obj.value %></a>' +	
+			'<% }); %>' +  		
 		'</nav>' +
     
-		'<nav class="btn-group">'+
+		'<nav class="btn-group">'+	
 			'<% _.each(arr, function(obj, index, arr) { %>' +
-	    	'<a class="<%= obj.class %>"><%= obj.value %></a>' +
-	    '<% }); %>' +  
+	    			'<a class="<%= obj.class %>"><%= obj.value %></a>' +
+	    		'<% }); %>' +  
 		'</nav>' +
   
 		'<nav class="btn-group">'+
 			'<% _.each(arr, function(obj, index, arr) { %>' +
-	    	'<a class="<%= obj.class %>"><%= obj.value %></a>' +
-	    '<% }); %>' +  
+	    			'<a class="<%= obj.class %>"><%= obj.value %></a>' +	    
+			'<% }); %>' +  
 		'</nav>' +
 	
 	'</header>'
-
 );
