177088c28603c08ac8f660e22d917a6c0927f1d2
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-06-24 16:08:55 -0700

    create header.js

---
+++ backbone/scripts/templates/header.js
@@ -0,0 +1,25 @@
+var HeaderTemplate = _.template(
+	
+  '<header class="navbar" id="header">'+
+	
+    '<nav class="btn-group">'+	
+			'<% _.each(arr, function(obj, index, arr) { %>' +
+	    	'<a class="<%= obj.class %>"><%= obj.value %></a>' +
+	    '<% }); %>' +  
+		'</nav>' +
+    
+		'<nav class="btn-group">'+
+			'<% _.each(arr, function(obj, index, arr) { %>' +
+	    	'<a class="<%= obj.class %>"><%= obj.value %></a>' +
+	    '<% }); %>' +  
+		'</nav>' +
+  
+		'<nav class="btn-group">'+
+			'<% _.each(arr, function(obj, index, arr) { %>' +
+	    	'<a class="<%= obj.class %>"><%= obj.value %></a>' +
+	    '<% }); %>' +  
+		'</nav>' +
+	
+	'</header>'
+
+);
