c38c4efbc538cc7b2ecb7934fb94a0cca33cfa3a
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-07-03 17:04:13 -0700

    Update worker.js

--- browser/src/worker.js
+++ browser/src/worker.js
@@ -1,8 +1,29 @@
+/* document */
+var blob = new Blob(
+    [].map.call(
+        document.querySelectorAll('script[type=\'text\/js-worker\']'), 
+        function (script) {  
+            return script.textContent;  
+        }), { type: 'text/javascript;charset=UTF-8'	 }
+);
+document.worker = new Worker(window.URL.createObjectURL(blob));
+document.worker.onmessage = function(event) { }
+/**/
+
+/* Function */
+function fn2workerURL(fn) {
+    var blob = new Blob(['('+fn.toString()+')()'], {	
+        type: 'application/javascript'		
+    })  	
+    return URL.createObjectURL(blob)	
+}
+/**/
+
+/* XHR */
 self.onmessage = function(event){
     var msg = event.data;
     getjson(msg);
 }
-
 function getjson(url){
   const request = new XMLHttpRequest();
   request.open("GET", url);
@@ -14,3 +35,4 @@ function getjson(url){
     self.postMessage(parser);
   }
 }
+/**/
