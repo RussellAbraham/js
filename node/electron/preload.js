d0975a1d745833545d5e57585fad0d43ca625be1
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-07-13 02:59:44 -0700

    update

--- node/electron/preload.js
+++ node/electron/preload.js
@@ -52,4 +52,31 @@ document.addEventListener('DOMContentLoaded', function(event){
   
   
   })
+  It should be possible with a combination of the child_process functions (e.g exec, execFile, spawn) and the Android SDK. Just use the child_process functions to execute the command for creating the .apk file.
+
+apk build command: “ant -Dsdk.dir= debug” (old command based on eclipse) .
+
+Check the Android SDK documentation for the latest information on building the apk from the command line: http://developer.android.com/tools/building/building-cmdline.html 332
+
+Example:
+
+Render process (index.html)
+<script type="text/javascript">
+    var ipc = require('ipc');
+    ipc.send('create_apk', 1);
+
+	ipc.on('create_apk_response', function(arg) {
+ 		document.getElementById('div').innerHTML = "apk file name: " + arg;
+	});
+    </script>
+main process (main.js)
+var ipc = require('ipc');
+ipc.on('create_apk', function(event, arg) {
+  var e = require('child_process');
+  e.exec('ant -Dsdk.dir='+ ANDROID_SDK_PATH +' debug', function(err,stdout, stderr) {
+  // process stdout, stderr printouts and get the apk filename if successfull
+  // return the apk file name in stdout
+  event.sender.send('create_apk_response', stdout);
+  });
+});
    */
