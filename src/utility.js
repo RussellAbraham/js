62b59b2985b0865b7b76e0445f88304a75aa7677
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-07-23 05:55:07 -0700

    update

--- src/utility.js
+++ src/utility.js
@@ -118,30 +118,20 @@ var createAssigner = function (keysFunc, undefinedOnly) {
   };
 };
 
+/* *** optimizCallback() *** */
 var optimizCallback = function (func, context, argCount) {
   if (context === void 0) return func;
   switch (argCount == null ? 3 : argCount) {
-    case 1:
-      return function (value) {
-        return func.call(context, value);
-      };
-    case 2:
-      return function (value, other) {
-        return func.call(context, value, other);
-      };
-    case 3:
-      return function (value, index, collection) {
-        return func.call(context, value, index, collection);
-      };
-    case 4:
-      return function (accumulator, value, index, collection) {
-        return func.call(context, accumulator, value, index, collection);
-      };
+    case 1: return function (value) { return func.call(context, value); };
+    case 2: return function (value, other) { return func.call(context, value, other); };
+    case 3: return function (value, index, collection) { return func.call(context, value, index, collection); };
+    case 4: return function (accumulator, value, index, collection) { return func.call(context, accumulator, value, index, collection); };
   }
   return function () {
     return func.apply(context, arguments);
   };
 };
+
 /* *** times() *** */
 function times(n, iteratee, context) {
   var accum = Array(Math.max(0, n));
