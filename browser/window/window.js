b2338a41e4929cce885bcd5a1f8f520c44369ad8
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-08-05 15:15:03 -0700

    Update
    Added 2 functions to test using window.open

--- browser/window/window.js
+++ browser/window/window.js
@@ -19,3 +19,27 @@
         resizeBy    
     ])   
 })();
+
+// testing window.open()
+
+function test1() {	
+	var oPrntWin = window.open("","_blank","width=450,height=470,left=400,top=100,menubar=yes,toolbar=no,location=no,scrollbars=yes");
+	oPrntWin.document.open();
+  oPrntWin.document.write("<!doctype html><html><head><title>Print<\/title><\/head><body onload=\"\">" + document.title + "<\/body><\/html>");
+  oPrntWin.document.close();
+}
+
+// likely to be more computationally expensive with variable lookup and eval, I have not noticed.. with is very usefull
+function test2() {
+	with(window.open("","_blank","width=450,height=470,left=400,top=100,menubar=yes,toolbar=no,location=no,scrollbars=yes")){
+		document.write(
+			'<!DOCTYPE html>' +
+			'<html>' +
+			'<head></head>' +
+			'<body></body>' +
+			'<html>'
+		);
+		document.close();
+	}
+	void 0
+}
