f2a88fe62787011167f9d6ffa00d804f9ad7c04f
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-08-10 00:21:12 -0700

    Update index.js
    Setup as prototype chain with closure to return an app object

--- index.js
+++ index.js
@@ -1,15 +1,62 @@
-
-
-(function(){
+(function(application, root){    
     
     'use strict';
-
-    const Index = function(name){
-        this.storage = new app.store(name);
-        this.model = new app.model(this.storage());
-        this.view = new app.view(this.template());
+    
+    /* internal */
+    function defineProperty(obj, key, value){ 
+	    if (key in obj) { 
+		    Object.defineProperty(obj, key, {  
+			    value: value,  
+			    enumerable: true,  
+			    configurable: true, 
+			    writable: true  
+		    });  
+	    } else {  
+    		obj[key] = value;  
+	    } return obj;   
+    }    
+    
+    /* todo : if setting a dispatch object for a collection, this is an optimized approach */
+    var optimizCallback = function (func, context, argCount) {
+        if (context === void 0) return func;
+        switch (argCount == null ? 3 : argCount) {
+            case 1: return function (value) { return func.call(context, value); };
+            case 2: return function (value, other) { return func.call(context, value, other); };
+            case 3: return function (value, index, collection) { return func.call(context, value, index, collection); };
+            case 4: return function (accumulator, value, index, collection) { return func.call(context, accumulator, value, index, collection); };
+        }
+        return function () {
+            return func.apply(context, arguments);
+        };
+    };
+    
+    function Demo(context, callback){
+        if(context){ this.context = context }
+        this.callback = callback;
     }
-
-    const index = new Index('local-index');
-
-})();
+    
+    Demo.prototype = {
+        context : null,
+        callback : null
+    }
+    
+    /* execute callback with object as parameter within the context   */
+    Demo.call = function(object){
+        if(object.callback.call(object.context === false)){
+             return false;
+        }
+        return true;
+    }
+    
+    /* define addition prototypes to chain */
+    application['api'] = function(){
+        return {
+            Demo : Demo
+        }
+    }
+    
+    if(typeof root !== 'undefined'){
+        root.app = application.api();
+    }
+    
+})(new Object(), this);
