7934aa9cff3a4d8527652eb918291f25d2f693df
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-08-11 18:45:58 -0700

    update index.js
    Added template function I'll use to build the docs for this repo.

--- index.js
+++ index.js
@@ -1,62 +1,88 @@
-(function(application, root){    
+/* todo : setup documentation templates */
+
+var _ = (function(_){
+	
+	_.defaults = function (object) {  
+		if (!object) { return object }    
+		for (var argsIndex = 1, argsLength = arguments.length; argsIndex < argsLength; argsIndex += 1) {    
+			var iterable = arguments[argsIndex];      
+			if (iterable) {      
+				for (var key in iterable) {        
+					if (object[key] == null) {          
+						object[key] = iterable[key]            
+					}          
+				}        
+			}      
+		}    
+		return object    
+	};
+  
+	_.templateSettings = {  
+		evaluate: /<%([\s\S]+?)%>/g,    
+		interpolate: /<%=([\s\S]+?)%>/g,    
+		escape: /<%-([\s\S]+?)%>/g    
+	};
+  
+	var noMatch = /(.)^/;
+  
+	var escapes = {  
+		"'": "'",    
+		'\\': '\\',    
+		'\r': 'r',    
+		'\n': 'n',    
+		'\t': 't',    
+		'\u2028': 'u2028',    
+		'\u2029': 'u2029'    
+	};
     
-    'use strict';
-    
-    /* internal */
-    function defineProperty(obj, key, value){ 
-	    if (key in obj) { 
-		    Object.defineProperty(obj, key, {  
-			    value: value,  
-			    enumerable: true,  
-			    configurable: true, 
-			    writable: true  
-		    });  
-	    } else {  
-    		obj[key] = value;  
-	    } return obj;   
-    }    
-    
-    /* todo : if setting a dispatch object for a collection, this is an optimized approach */
-    var optimizCallback = function (func, context, argCount) {
-        if (context === void 0) return func;
-        switch (argCount == null ? 3 : argCount) {
-            case 1: return function (value) { return func.call(context, value); };
-            case 2: return function (value, other) { return func.call(context, value, other); };
-            case 3: return function (value, index, collection) { return func.call(context, value, index, collection); };
-            case 4: return function (accumulator, value, index, collection) { return func.call(context, accumulator, value, index, collection); };
-        }
-        return function () {
-            return func.apply(context, arguments);
-        };
-    };
-    
-    function Demo(context, callback){
-        if(context){ this.context = context }
-        this.callback = callback;
-    }
-    
-    Demo.prototype = {
-        context : null,
-        callback : null
-    }
-    
-    /* execute callback with object as parameter within the context   */
-    Demo.call = function(object){
-        if(object.callback.call(object.context === false)){
-             return false;
-        }
-        return true;
-    }
-    
-    /* define addition prototypes to chain */
-    application['api'] = function(){
-        return {
-            Demo : Demo
-        }
-    }
-    
-    if(typeof root !== 'undefined'){
-        root.app = application.api();
-    }
-    
-})(new Object(), this);
+	var escaper = /\\|'|\r|\n|\t|\u2028|\u2029/g;
+  
+	_.template = function (text, data, settings) {  
+		var render;    
+		settings = _.defaults({}, settings, _.templateSettings);    
+		var matcher = new RegExp([    
+			(settings.escape || noMatch).source,      		
+			(settings.interpolate || noMatch).source,      
+			(settings.evaluate || noMatch).source      
+		].join('|') + '|$', 'g');    
+		var index = 0;    
+		var source = "__p+='";    
+		text.replace(matcher, function (match, escape, interpolate, evaluate, offset) {    
+			source += text      
+				.slice(index, offset)        
+				.replace(escaper, function (match) {        
+				return '\\' + escapes[match]        		
+			});      
+			if (escape) {
+				source += "'+\n((__t=(" + escape + "))==null?'':_.escape(__t))+\n'"        
+			}
+			if (interpolate) {
+				source += "'+\n((__t=(" + interpolate + "))==null?'':__t)+\n'"        
+			}                
+			if (evaluate) {
+				source += "';\n" + evaluate + "\n__p+='"
+			}
+			index = offset + match.length;
+			return match
+		});
+		source += "';\n";    
+		if (!settings.variable) {
+			source = 'with(obj||{}){\n' + source + '}\n'
+		}
+		source = "var __t,__p='',__j=Array.prototype.join,print=function(){__p+=__j.call(arguments" +
+			",'');};\n" + source + "return __p;\n";
+		try {
+			render = new Function(settings.variable || 'obj', '_', source)
+		} catch (e) {
+			e.source = source;
+			throw e
+		}
+		if (data) { return render(data, _) }
+		var template = function (data) {
+			return render.call(this, data, _)
+		};
+		template.source = 'function(' + (settings.variable || 'obj') + '){\n' + source + '}';
+		return template
+	};
+	return _
+})({});
