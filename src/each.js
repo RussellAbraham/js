2c30570c2042dcdd5c879c6bec64e845e7ddd340
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-08-29 17:12:47 -0700

    update
    1. [ each, map, keys and values ] helper functions
    
    2. different iife composites

--- src/each.js
+++ src/each.js
@@ -1,54 +1,89 @@
-
 /**
- * @function { each }
- * iterate over array or object data type
+ * @param  object 
+ * @param  iterator
+ * @param  context 
  */
-
-function each(object, iterator, context){
-    
-    if(object == null){ return }
-
-    // If type is array and no context object, use the native forEach 
-    if([].forEach && object.forEach === [].forEach){
-      object.forEach(iterator, context);  
-    } 
-
-    // If type is array and context object is a parameter, iterator calls function on each context object     
-    else if (object.length === +object.length) {
-        for(var i = 0, l = object.length; i < l;i++){
-            if(i in object && iterator.call(context, object[i], i, object) === breaker) return;
-        }
+function each(object, iterator, context) {
+    if (object == null) {
+        return
     }
-
-    // If type is object, iterate over the keys 
-    else {
-        for(var key in object){
-            if({}.hasOwnProperty.call(object, key)){
-                if(iterator.call(context, object[key], key, object) === {}) return;
+    if ([].forEach && object.forEach === [].forEach) {
+        object.forEach(iterator, context);
+    } else if (object.length === +object.length) {
+        for (var i = 0, l = object.length; i < l; i++) {
+            if (i in object && iterator.call(context, object[i], i, object) === breaker) return;
+        }
+    } else {
+        for (var key in object) {
+            if ({}.hasOwnProperty.call(object, key)) {
+                if (iterator.call(context, object[key], key, object) === {}) return;
             }
         }
     }
-     
 }
 
-// function reverse(array){
-//    for(var i = array.length - 1; i >= 0; i--){
-//
-//    }
-// }
+/** 
+ * @param object
+ * @param iterator
+ * @param context
+ */
+function map(object, iterator, context) {
+    var results = [];
+    if (object == null) return results;
+    if ([].map && object.map === [].map) return object.map(iterator, context);
+    each(object, function (value, index, list) {
+        return results[results.length] = iterator.call(context, value, index, list);
+    });
+    return results;
+}
+
+function keys(object) {
+    if (object !== Object(object)) throw new TypeError('Invalid object');
+    var keys = [];
+    for (var key in object)
+        if ({}.hasOwnProperty.call(object, key)) keys[keys.length] = key;
+    return keys;
+}
+
+function isObject(obj) {
+    return obj === Object(obj);
+}
+
+function allKeys(object) {
+    if (!isObject(object)) {
+        return [];
+    }
+    var keys = [];
+    for (var key in object) {
+        keys.push(key);
+    }
+    return keys;
+}
+
+function identity(object) {
+    return object;
+}
+
+function values(object) {
+    return map(object, identity);
+}
 
 
-// try a reverse iteration
 
-function eachReverse(object, iterator, context){
+/**@function eachReverse
+ * @param { object }
+ * @param { iterator }
+ * @param { context }
+ */
 
-    if(object == null){ return }
+function eachReverse(object, iterator, context) {
 
-    else if(object.length === +object.length){
-        for(var i = object.length - 1; i > -1; i--){
-            if(object[i] && iterator.call(context, object[i], i, object) === breaker) return;
+    if (object == null) {
+        return
+    } else if (object.length === +object.length) {
+        for (var i = object.length - 1; i > -1; i--) {
+            if (object[i] && iterator.call(context, object[i], i, object) === breaker) return;
         }
     }
 
 }
-
