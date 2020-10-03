724e12948f8cf01bbef7147b69935cadcac7c608
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-10-03 14:10:06 -0700

    update deckOfCards.js
    Replaced random access array from the first test of iterations with a static method. SinglyLinkedList.
    Obviously 1mill iterations is no longer a possibility however that was far beyond the performance I would hope for.
    Reducing the iterations to 100*100 and the compilation time is almost instant. This example builds 52000 decks of cards under on name space.

--- src/deckOfCards.js
+++ src/deckOfCards.js
@@ -1,67 +1,245 @@
-/* *** optimizCallback() *** */
-var optimizCallback = function (func, context, argCount) {
-  if (context === void 0) return func;
-  switch (argCount == null ? 3 : argCount) {
-    case 1: return function (value) { return func.call(context, value); };
-    case 2: return function (value, other) { return func.call(context, value, other); };
-    case 3: return function (value, index, collection) { return func.call(context, value, index, collection); };
-    case 4: return function (accumulator, value, index, collection) { return func.call(context, accumulator, value, index, collection); };
-  }
-  return function () {
-    return func.apply(context, arguments);
-  };
-};
-/* *** times() *** */
-function times(n, iteratee, context) {
-  var accum = Array(Math.max(0, n));
-  iteratee = optimizCallback(iteratee, context, 1);
-  for (var i = 0; i < n; i++) accum[i] = iteratee(i);
-  return accum;
-};
+"use strict";
 
-const suits = ['hearts', 'clubs', 'diamonds', 'spades'];
-const values = '2 3 4 5 6 7 8 9 10 J Q K A'.split(' ');
-	
-const limit = values.length * suits.length;
-	
-function offs(){
-	const deck = [];
-	for (let value of values) {
-		for (let suit of suits) {
-			deck.push({value:value,suit:suit})
-		}
-	}
-	return deck;
+function _defineProperties(target, props) {
+  for (var i = 0; i < props.length; i++) {
+    var descriptor = props[i];
+    descriptor.enumerable = descriptor.enumerable || false;
+    descriptor.configurable = true;
+    if ("value" in descriptor) descriptor.writable = true;
+    Object.defineProperty(target, descriptor.key, descriptor);
+  }
 }
 
-function mins(){
-	const deck = [];
+function _createClass(Constructor, protoProps, staticProps) {
+  if (protoProps) _defineProperties(Constructor.prototype, protoProps);
+  if (staticProps) _defineProperties(Constructor, staticProps);
+  return Constructor;
+}
+
+function _classCallCheck(instance, Constructor) {
+  if (!(instance instanceof Constructor)) {
+    throw new TypeError("Cannot call a class as a function")
+  }
+};
+
+var Node = function Node(val) {
+  _classCallCheck(this, Node);
+
+  this.val = val;
+  this.next = null;
+};
+
+var SinglyLinkedList = /*#__PURE__*/ function () {
+  function SinglyLinkedList() {
+    _classCallCheck(this, SinglyLinkedList);
+
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  _createClass(SinglyLinkedList, [{
+    key: "push",
+    value: function push(val) {
+      var newNode = new Node(val);
+
+      if (!this.head) {
+        this.head = newNode;
+        this.tail = this.head;
+      } else {
+        this.tail.next = newNode;
+        this.tail = newNode;
+      }
+
+      this.length++;
+      return this;
+    }
+  }, {
+    key: "pop",
+    value: function pop() {
+      if (!this.head) return undefined;
+      var current = this.head;
+      var newTail = current;
+
+      while (current.next) {
+        newTail = current;
+        current = current.next;
+      }
+
+      this.tail = newTail;
+      this.tail.next = null;
+      this.length--;
+
+      if (this.length === 0) {
+        this.head = null;
+        this.tail = null;
+      }
+
+      return current;
+    }
+  }, {
+    key: "shift",
+    value: function shift() {
+      if (!this.head) return undefined;
+      var currentHead = this.head;
+      this.head = currentHead.next;
+      this.length--;
+
+      if (this.length === 0) {
+        this.tail = null;
+      }
+
+      return currentHead;
+    }
+  }, {
+    key: "unshift",
+    value: function unshift(val) {
+      var newNode = new Node(val);
+
+      if (!this.head) {
+        this.head = newNode;
+        this.tail = this.head;
+      }
+
+      newNode.next = this.head;
+      this.head = newNode;
+      this.length++;
+      return this;
+    }
+  }, {
+    key: "get",
+    value: function get(index) {
+      if (index < 0 || index >= this.length) return null;
+      var counter = 0;
+      var current = this.head;
+
+      while (counter !== index) {
+        current = current.next;
+        counter++;
+      }
+
+      return current;
+    }
+  }, {
+    key: "set",
+    value: function set(index, val) {
+      var foundNode = this.get(index);
+
+      if (foundNode) {
+        foundNode.val = val;
+        return true;
+      }
+
+      return false;
+    }
+  }, {
+    key: "insert",
+    value: function insert(index, val) {
+      if (index < 0 || index > this.length) return false;
+      if (index === this.length) return !!this.push(val);
+      if (index === 0) return !!this.unshift(val);
+      var newNode = new Node(val);
+      var prev = this.get(index - 1);
+      var temp = prev.next;
+      prev.next = newNode;
+      newNode.next = temp;
+      this.length++;
+      return true;
+    }
+  }, {
+    key: "remove",
+    value: function remove(index) {
+      if (index < 0 || index >= this.length) return undefined;
+      if (index === 0) return this.shift();
+      if (index === this.length - 1) return this.pop();
+      var previousNode = this.get(index - 1);
+      var removed = previousNode.next;
+      previousNode.next = removed.next;
+      this.length--;
+      return removed;
+    }
+  }, {
+    key: "reverse",
+    value: function reverse() {
+      var node = this.head;
+      this.head = this.tail;
+      this.tail = node;
+      var next;
+      var prev = null;
+
+      for (var i = 0; i < this.length; i++) {
+        next = node.next;
+        node.next = prev;
+        prev = node;
+        node = next;
+      }
+
+      return this;
+    }
+  }, {
+    key: "print",
+    value: function print() {
+      var arr = [];
+      var current = this.head;
+
+      while (current) {
+        arr.push(current.val);
+        current = current.next;
+      }
+
+      console.log(arr);
+
+    }
+  }]);
+
+  return SinglyLinkedList;
+}();
+
+const suits = ["hearts", "clubs", "diamonds", "spades"];
+const values = "2 3 4 5 6 7 8 9 10 J Q K A".split(" ");
+
+const limit = values.length * suits.length;
+
+
+/* operation test using es6 for of */
+const deck1 = new SinglyLinkedList();
+function offs() {
+	for (let value of values) {
+		for (let suit of suits) {
+			deck1.push({ value: value, suit: suit });
+		}
+	}
+	return deck1;
+}
+
+/* operation test using for length minus */
+const deck2 = new SinglyLinkedList();
+function mins() {
 	let i;
 	for (i = limit - 1; i >= 0; i--) {
 		let inst = values[Math.floor(i / suits.length)];
 		let g = suits[Math.floor(i % suits.length)];
-		deck.push({'suit':g,'value':inst})
-	}	
-	return deck;
+		deck2.push({ suit: g, value: inst });
+	}
+	return deck2;
 }
-function testmin(){
+
+function testmin() {
 	let a = performance.now();
-	_.times(10000, mins);
+	_.times(100, mins);
+	let b = performance.now();
+	console.log(b - a);
+}
+function testof() {
+	let a = performance.now();
+	_.times(100, offs);
 	let b = performance.now();
 	console.log(b - a);
 }
 
 testmin = _.memoize(testmin);
-
-function testof(){
-	let a = performance.now();
-	_.times(10000, offs);
-	let b = performance.now();
-	console.log(b - a);
-}
-
 testof = _.memoize(testof);
 
-function test(cb){
-	_.times(1000, cb);
+function test(cb) {
+	_.times(100, cb);
 }
