8d2d998b919568b5e05b8c3b2207c6e50707d01d
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-10-02 18:13:16 -0700

    Update
    Memoize main loops

--- src/deckOfCards.js
+++ src/deckOfCards.js
@@ -24,27 +24,6 @@ const values = '2 3 4 5 6 7 8 9 10 J Q K A'.split(' ');
 	
 const limit = values.length * suits.length;
 	
-function offs(){
-	const deck = [];
-	for (let value of values) {
-		for (let suit of suits) {
-			deck.push({value:value,suit:suit})
-		}
-	}
-	return deck;
-}
-
-function mins(){
-	const deck = [];
-	let i;
-	for (i = limit - 1; i >= 0; i--) {
-		let inst = values[Math.floor(i / suits.length)];
-		let g = suits[Math.floor(i % suits.length)];
-		deck.push({'suit':g,'value':inst})
-	}	
-	return deck;
-}
-	
 
 function testmin(){
 	let a = performance.now();
@@ -53,12 +32,16 @@ function testmin(){
 	console.log(b - a);
 }
 
+testmin = _.memoize(testmin);
+
 function testof(){
 	let a = performance.now();
 	_.times(10000, offs);
 	let b = performance.now();
 	console.log(b - a);
 }
+
+testof = _.memoize(testof);
 
 function test(cb){
 	_.times(1000, cb);
