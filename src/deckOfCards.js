fc8fbde91ddd895786d9cad4c832af4d24826b6b
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-10-02 17:57:22 -0700

    Update
    added the utility function used for the test.

--- src/deckOfCards.js
+++ src/deckOfCards.js
@@ -1,3 +1,23 @@
+/* *** optimizCallback() *** */
+var optimizCallback = function (func, context, argCount) {
+  if (context === void 0) return func;
+  switch (argCount == null ? 3 : argCount) {
+    case 1: return function (value) { return func.call(context, value); };
+    case 2: return function (value, other) { return func.call(context, value, other); };
+    case 3: return function (value, index, collection) { return func.call(context, value, index, collection); };
+    case 4: return function (accumulator, value, index, collection) { return func.call(context, accumulator, value, index, collection); };
+  }
+  return function () {
+    return func.apply(context, arguments);
+  };
+};
+/* *** times() *** */
+function times(n, iteratee, context) {
+  var accum = Array(Math.max(0, n));
+  iteratee = optimizCallback(iteratee, context, 1);
+  for (var i = 0; i < n; i++) accum[i] = iteratee(i);
+  return accum;
+};
 
 const suits = ['hearts', 'clubs', 'diamonds', 'spades'];
 const values = '2 3 4 5 6 7 8 9 10 J Q K A'.split(' ');
