b6150e917272d5267eb1c2422950544fae58809f
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-10-02 17:01:32 -0700

    Update deckOfCards.js

--- src/deckOfCards.js
+++ src/deckOfCards.js
@@ -31,3 +31,29 @@ function build() {
 		output: output
 	};
 }
+
+const makeDeck = () => {
+  return {
+    deck: [],
+    suits: ['hearts', 'diamonds', 'spades', 'clubs'],
+    values: '2,3,4,5,6,7,8,9,10,J,Q,K,A',
+    initializeDeck() {
+      const {
+        suits,
+        values,
+        deck
+      } = this;
+      for (let value of values.split(',')) {
+        for (let suit of suits) {
+          deck.push({
+            value,
+            suit
+          })
+        }
+      }
+      // return deck;
+    }
+  }
+}
+const myDeck = makeDeck();
+myDeck.initializeDeck();
