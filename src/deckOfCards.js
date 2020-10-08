a4ce3985f7af94ab5093983e053168c8eff7403a
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-10-07 21:38:20 -0700

    Update

--- src/deckOfCards.js
+++ src/deckOfCards.js
@@ -1,38 +1,6 @@
-
-const suits = ["hearts", "clubs", "diamonds", "spades"];
-
-const values = "2 3 4 5 6 7 8 9 10 J Q K A".split(" ");
-
-const limit = values.length * suits.length;
-
-
-/* operation test using for length minus */
-const decks = [];
-
-function buildDeck() {
+// @ line 237
+"use strict";
 	
-	let i, 			
-	    deck = [],	
-	    slen = suits.length;
-	
-	for (i = limit - 1; i >= 0; i--) {		
-		let value = values[Math.floor(i / slen)];		
-		let suit = suits[Math.floor(i % slen)];		
-		deck.push({ 
-			suit : suit, 	
-			value : value 
-		});		
-	}
-	
-	return decks.push(
-		JSON.stringify(deck)
-	);
-	
-}
-
-
-// Factory Optimized
-
 var optimizCallback = function (func, context, argCount) {
   if (context === void 0) return func;
   switch (argCount == null ? 3 : argCount) {
@@ -67,11 +35,230 @@ function memoize(callback, address){
   return function(){
     key = address.apply(this, arguments);
     return has(cache, key) ? cache[key] : (cache[key] = callback.apply(this, arguments));
+	}
+}
+	
+function _defineProperties(target, props) {
+  for (var i = 0; i < props.length; i++) {
+    var descriptor = props[i];
+    descriptor.enumerable = descriptor.enumerable || false;
+    descriptor.configurable = true;
+    if ("value" in descriptor) descriptor.writable = true;
+    Object.defineProperty(target, descriptor.key, descriptor);
   }
 }
 
-mins = memoize(mins);
+function _createClass(Constructor, protoProps, staticProps) {
+  if (protoProps) _defineProperties(Constructor.prototype, protoProps);
+  if (staticProps) _defineProperties(Constructor, staticProps);
+  return Constructor;
+}
 
-function buildDecks(num){
-	times(num, mins);
+function _classCallCheck(instance, Constructor) {
+  if (!(instance instanceof Constructor)) {
+    throw new TypeError("Cannot call a class as a function")
+  }
+};
+
+var Node = function Node(val) {
+  _classCallCheck(this, Node);
+
+  this.val = val;
+  this.next = null;
+};
+
+var SinglyLinkedList = /*#__PURE__*/ function () {
+  function SinglyLinkedList() {
+    _classCallCheck(this, SinglyLinkedList);
+
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  _createClass(SinglyLinkedList, [{
+    key: "push",
+    value: function push(val) {
+      var newNode = new Node(val);
+
+      if (!this.head) {
+        this.head = newNode;
+        this.tail = this.head;
+      } else {
+        this.tail.next = newNode;
+        this.tail = newNode;
+      }
+
+      this.length++;
+      return this;
+    }
+  }, {
+    key: "pop",
+    value: function pop() {
+      if (!this.head) return undefined;
+      var current = this.head;
+      var newTail = current;
+
+      while (current.next) {
+        newTail = current;
+        current = current.next;
+      }
+
+      this.tail = newTail;
+      this.tail.next = null;
+      this.length--;
+
+      if (this.length === 0) {
+        this.head = null;
+        this.tail = null;
+      }
+
+      return current;
+    }
+  }, {
+    key: "shift",
+    value: function shift() {
+      if (!this.head) return undefined;
+      var currentHead = this.head;
+      this.head = currentHead.next;
+      this.length--;
+
+      if (this.length === 0) {
+        this.tail = null;
+      }
+
+      return currentHead;
+    }
+  }, {
+    key: "unshift",
+    value: function unshift(val) {
+      var newNode = new Node(val);
+
+      if (!this.head) {
+        this.head = newNode;
+        this.tail = this.head;
+      }
+
+      newNode.next = this.head;
+      this.head = newNode;
+      this.length++;
+      return this;
+    }
+  }, {
+    key: "get",
+    value: function get(index) {
+      if (index < 0 || index >= this.length) return null;
+      var counter = 0;
+      var current = this.head;
+
+      while (counter !== index) {
+        current = current.next;
+        counter++;
+      }
+
+      return current;
+    }
+  }, {
+    key: "set",
+    value: function set(index, val) {
+      var foundNode = this.get(index);
+
+      if (foundNode) {
+        foundNode.val = val;
+        return true;
+      }
+
+      return false;
+    }
+  }, {
+    key: "insert",
+    value: function insert(index, val) {
+      if (index < 0 || index > this.length) return false;
+      if (index === this.length) return !!this.push(val);
+      if (index === 0) return !!this.unshift(val);
+      var newNode = new Node(val);
+      var prev = this.get(index - 1);
+      var temp = prev.next;
+      prev.next = newNode;
+      newNode.next = temp;
+      this.length++;
+      return true;
+    }
+  }, {
+    key: "remove",
+    value: function remove(index) {
+      if (index < 0 || index >= this.length) return undefined;
+      if (index === 0) return this.shift();
+      if (index === this.length - 1) return this.pop();
+      var previousNode = this.get(index - 1);
+      var removed = previousNode.next;
+      previousNode.next = removed.next;
+      this.length--;
+      return removed;
+    }
+  }, {
+    key: "reverse",
+    value: function reverse() {
+      var node = this.head;
+      this.head = this.tail;
+      this.tail = node;
+      var next;
+      var prev = null;
+
+      for (var i = 0; i < this.length; i++) {
+        next = node.next;
+        node.next = prev;
+        prev = node;
+        node = next;
+      }
+
+      return this;
+    }
+  }, {
+    key: "print",
+    value: function print() {
+      var arr = [];
+      var current = this.head;
+
+      while (current) {
+        arr.push(current.val);
+        current = current.next;
+      }
+
+      console.log(arr);
+
+    }
+  }]);
+
+  return SinglyLinkedList;
+}();
+
+
+const decks = new SinglyLinkedList();
+
+const suits = ["hearts", "clubs", "diamonds", "spades"];
+
+const values = "2 3 4 5 6 7 8 9 10 J Q K A".split(" ");
+
+const limit = values.length * suits.length;
+
+function buildDeck() {
+	
+	let i, 			
+	    deck = [],	
+	    slen = suits.length;
+	
+	for (i = limit - 1; i >= 0; i--) {		
+		let value = values[Math.floor(i / slen)];		
+		let suit = suits[Math.floor(i % slen)];		
+		deck.push({ 
+			suit : suit, 	
+			value : value 
+		});		
+	}
+	
+	return decks.push(
+		JSON.stringify(deck)
+	);
+	
 }
