258e9860ae27a35f204bdf8eafcf042c4c83b930
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-10-23 23:25:38 -0700

    Update canvas.js

--- src/canvas.js
+++ src/canvas.js
@@ -1,3 +1,65 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('webgl');
+
+function peek(){
+	Object.values([ctx.__proto__]).forEach(function(keys){
+		Object.keys(keys).forEach(function(values, index){
+			console.log(''.concat(index, ' : ', values));
+		})
+	})
+}
+
+// crossorigin image request
+function Texture(src){
+	this.image = new Image();
+	this.image.src = src;
+	this.image.crossOrigin = "Anonymous";
+}
+
+function Line(options){
+	options = (options || {});
+	this.ctx = options.ctx;
+	this.startX = options.startX;
+	this.startY = options.startY; 
+	this.endX = options.endX;
+	this.endY = options.endY;
+	//this.color = options.color
+}
+
+Line.prototype.draw = function(){
+	this.ctx.beginPath();
+	this.ctx.moveTo(this.startX,this.startY);
+	this.ctx.lineTo(this.endX,this.endY);
+	this.ctx.stroke();
+}
+
+function Arc(options){
+	options = (options || {});
+	this.ctx = options.ctx;
+	this.centerX = options.centerX;
+	this.centerY = options.centerY; 
+	this.radius = options.radius;
+	this.startAngle = options.startAngle;
+	this.endAngle = options.endAngle;
+}
+
+Arc.prototype.draw = function(){
+	this.options.ctx.beginPath();
+	this.options.ctx.arc(this.options.centerX, this.options.centerY, this.options.radius, this.options.startAngle, this.options.endAngle);
+	this.options.ctx.stroke();	
+}
+
+const data = [
+	{startX : 0,startY : 0,endX : 100,endY : 100},
+	{startX : 0,startY : 0,endX : 100,endY : 100}
+]
+
+function lines(){
+	data.forEach(function(line){
+		var linetest = new Line();
+		linetest.draw();
+	});
+}
 const grid = [
     1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 
     1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 
@@ -25,66 +87,3 @@ const grid = [
     1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 
     1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1
 ];
-
-function Texture(src){
-	this.image = new Image();
-	this.image.src = src;
-	this.image.crossOrigin = "Anonymous";
-}
-
-function Line(options){
-	options = (options || {});
-	this.ctx = options.ctx;
-	this.startX = options.startX;
-	this.startY = options.startY; 
-	this.endX = options.endX;
-	this.endY = options.endY;
-	//this.color = options.color
-}
-
-const ctx = document.getElementById('canvas').getContext('2d');
-const data = [
-	{	
-		startX : 0,
-		startY : 0,
-		endX : 100,
-		endY : 100
-	},
-	{
-		startX : 0,
-		startY : 0,
-		endX : 100,
-		endY : 100
-	}
-]
-function lines(){
-	data.forEach(function(line){
-		var linetest = new Line();
-		linetest.draw();
-	});
-}
-
-
-
-Line.prototype.draw = function(){
-	this.ctx.beginPath();
-	this.ctx.moveTo(this.startX,this.startY);
-	this.ctx.lineTo(this.endX,this.endY);
-	this.ctx.stroke();
-}
-
-function Arc(options){
-	options = (options || {});
-	this.ctx = options.ctx;
-	this.centerX = options.centerX;
-	this.centerY = options.centerY; 
-	this.radius = options.radius;
-	this.startAngle = options.startAngle;
-	this.endAngle = options.endAngle;
-}
-
-Arc.prototype.draw = function(){
-	this.options.ctx.beginPath();
-	this.options.ctx.arc(this.options.centerX, this.options.centerY, this.options.radius, this.options.startAngle, this.options.endAngle);
-	this.options.ctx.stroke();	
-}
