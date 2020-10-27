c19fa4d82c175a83219773c34a69a43502d3aed1
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-10-27 01:15:37 -0700

    Update

--- src/element.js
+++ src/element.js
@@ -1,17 +1,55 @@
-/* var domNodeParent, domNodeChild, domNodeFragment = new DocumentFragment(); */
+const fragment = new DocumentFragment();
 
-function element(target, element, attrs, text){	
-	const myElement = document.createElement(element);
-	const myTextNode = document.createTextNode(text);
-	for(var attr in attrs){
-		myElement.setAttribute(attr, attrs[attr])
-	}
-  	if(text){
-		myElement.appendChild(myTextNode);
-	}
-	return target.appendChild(myElement);
-}
+let parent, child, io, types;
 
 function insertTextNodeBefore(text, node) {
-  node.insertBefore(text, node.childNodes[0]);
-} 
+	node.insertBefore(text, node.childNodes[0]);
+}
+
+/* v1 */
+function element(target, element, attrs, text) {
+	parent = document.createElement(element);
+	for (var attr in attrs) {
+		parent.setAttribute(attr, attrs[attr])
+	}
+	if (text) {
+		child = document.createTextNode(text);
+		parent.appendChild(child);
+	}
+	return target.appendChild(parent);
+}
+
+/* v2 */
+function element(target, element, options) {
+	
+	options = (options || {});
+	
+	parent = document.createElement(element);
+	
+	if(options.class){
+		parent.className = options.class;	
+	}
+	
+	if(options.text){
+		child = document.createTextNode(options.text);
+		parent.appendChild(child);
+	}
+	
+	return target.appendChild(parent);
+	
+}
+
+
+/* use */
+
+function node(object, string){
+  parent = element(fragment, 'ul', { class : '' });
+  child = element(parent, 'li', { class : '' });
+	io = element(child, 'code', object, string);
+  return main.appendChild(fragment);
+}
+
+node({
+	class:'a', 
+	text: 'test' 
+});
