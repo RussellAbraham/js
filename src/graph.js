baba74052247437b5489dacbb49084450a7b48e0
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-10-31 19:37:33 -0700

    Update graph.js
    JavaScript Graph,
    constructor,
    prototype

--- src/graph.js
+++ src/graph.js
@@ -1,79 +1,56 @@
-(function(root) {
-	var require, define;
+function Graph(){
+    this.adjacencyList = {};
+}
 
-	(function() {
-		var modules = {},
-			requireStack = [],
-			inProgressModules = {},
-			SEPARATOR = ".";
-		function build(module) {
-			var factory = module.factory;
-			var localRequire = function(id) {
-				var resultantId = id;
-				if (id.charAt(0) === ".") {
-					resultantId =
-						module.id.slice(0, module.id.lastIndexOf(SEPARATOR)) +
-						SEPARATOR +
-						id.slice(2);
-				}
-				return require(resultantId);
-			};
-			module.exports = {};
-			delete module.factory;
-			factory(localRequire, module.exports, module);
-			return module.exports;
-		}
-		require = function(id) {
-			if (!modules[id]) {
-				throw "module " + id + " not found";
-			} else if (id in inProgressModules) {
-				var cycle =
-					requireStack.slice(inProgressModules[id]).join("->") + "->" + id;
-				throw "Cycle in require graph: " + cycle;
-			}
-			if (modules[id].factory) {
-				try {
-					inProgressModules[id] = requireStack.length;
-					requireStack.push(id);
-					return build(modules[id]);
-				} finally {
-					delete inProgressModules[id];
-					requireStack.pop();
-				}
-			}
-			return modules[id].exports;
-		};
-		define = function(id, factory) {
-			if (modules[id]) {
-				throw "module " + id + " already defined";
-			}
-			modules[id] = {
-				id: id,
-				factory: factory
-			};
-		};
-		define.remove = function(id) {
-			delete modules[id];
-		};
-		define.moduleMap = modules;
-	})();
+Graph.prototype = {
+	
+    // setting constructor, new instance of Graph has Graph as its constructor
+    // without, constructor will be Object {}	
+	
+    constructor : Graph,
+	
+    addVertex : function(vertex){
+        if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+    },
 
-	if (typeof module === "object" && typeof require === "function") {
-		module.exports.require = require;
-		module.exports.define = define;
-	}
+    addEdge : function(v1,v2){
+        this.adjacencyList[v1].push(v2);
+        this.adjacencyList[v2].push(v1);        
+    },
 
-	if (typeof root !== "undefined") {
-		root.require = require;
-		root.define = define;
-	}
-})(this);
+    removeEdge : function(vertex1, vertex2){
+        this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(function(v){
+            return v !== vertex2;
+        });
+        this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(function(v){
+            return v !== vertex1;
+        });        
+    },
 
-define("test", function(require, exports, module) {
-	var test = {
-		log: function(string) {
-			return console.log(string);
-		}
-	};
-	module.exports = test;
-});
+    removeVertex : function(vertex){
+        while(this.adjacencyList[vertex].length){
+            var adjacentVertex = this.adjacencyList[vertex].pop();
+            this.removeEdge(vertex,adjacentVertex);
+        }
+        delete this.adjacencyList[vertex];
+    }
+
+}
+
+const g = new Graph();
+
+g.addVertex('left');
+g.addVertex('middle');
+g.addVertex('right');
+
+g.addEdge('left', 'middle');
+g.addEdge('right', 'middle');
+g.addEdge('left', 'right');
+
+/*
+Object {
+ left : ['middle', 'right'],
+ middle : ['left','right'],
+ right : ['middle','left']
+}
+*/
