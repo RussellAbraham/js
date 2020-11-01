8cea195abbfb0cc8196fbaeba7e31cd409d68e26
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-10-31 18:44:59 -0700

    Update file-reader.js
    Should read a range of bytes. 
    
    Constructor needs work
    
    https://codepen.io/RJLeyra/pen/GRqJRVX

--- src/file-reader.js
+++ src/file-reader.js
@@ -1,23 +1,59 @@
-// mostly pseudo code
+function Ftor(){
 
-function _cat() {
-  
-    // whatever object this is a partof has to pass in a string to determine what read method will be used 
-    var format = '';
-
-    // file reader constructor handles input
-    var cat = new FileReader();
-    
-    // blob constuctor handles output
-    var blob = new Blob([], {});
-
-    cat.onload = function(event){}
-
-    switch(format){
-        case 'txt':cat.readAsText(); break;
-        case 'bin':cat.readAsBinaryString(); break;
-        case 'arr':cat.readAsArrayBuffer(); break;
-        case 'url':cat.readAsDataURL(); break;
-    }
+	this.reader = new FileReader();
+	
+	this.reader.addEventListener('load', this.onReaderLoad.bind(this, true), false);	
+	
+	this.reader.addEventListener('loadend', this.onReaderLoadEnd.bind(this, true), false);
 
 }
+
+Ftor.prototype = {
+
+	load : function(options){
+	
+		options = (options || {});
+		
+		this.content = options.content;
+		
+		this.startbyte = options.startbye;
+		
+		this.stopbyte = options.stopbyte;
+		
+		this.id = options.id;
+		
+		var blob = new Blob([this.content], { type : 'application/octet-stream' }).slice(this.startbyte, this.stopbyte + 1);
+		
+		switch(this.id.toLowerCase()){				
+		
+			case 'arraybuffer' : this.reader.readAsArrayBuffer(blob); break;
+			
+			case 'blob' : this.reader.readAsBinaryString(blob); break;
+			
+			case 'text' : this.reader.readAsText(blob); break;
+			
+			case 'url' : this.reader.readAsDataURL(blob); break;
+	
+		}
+		
+	},
+
+	onReaderLoad : function(event){
+	
+		// console.log(this.reader.result);
+		
+	},
+	
+	onReaderLoadEnd : function(event){
+	
+		if (this.reader.readyState === FileReader.DONE) {
+		
+			document.getElementById('text').value = this.reader.result;
+			
+			console.log(this.reader.result);
+			
+		}			
+		
+	}		
+	
+};
