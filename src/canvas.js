11aa21dc352424e8d55681356ac2d74bfd007048
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-11-01 02:52:37 -0800

    Update canvas.js

--- src/canvas.js
+++ src/canvas.js
@@ -1,57 +1,17 @@
 const canvas = document.getElementById('canvas');
+const ctx2D = canvas.getContext('2d');
+const ctxGL = canvas.getContext('webgl');
+
 const ctx = canvas.getContext('webgl');
 
-// 1, no max iterator
-function peek(){
-	Object.values([ctx.__proto__]).forEach(function(keys){
+function peek(object){
+	Object.values([object.__proto__]).forEach(function(keys){
 		Object.keys(keys).forEach(function(values, index){
 			console.log(''.concat(index, ' : ', values));
 		})
 	})
 }
 
-// 2, no max iterator
-function getValues(obj) {
-
-    function each(obj, iterator, context) {
-        var breaker = {};
-        if (obj == null) return;
-        if ([].forEach && obj.forEach === [].forEach) {
-            obj.forEach(iterator, context);
-        } else if (obj.length === +obj.length) {
-            for (var i = 0, l = obj.length; i < l; i++) {
-                if (i in obj && iterator.call(context, obj[i], i, obj) === breaker) return;
-            }
-        } else {
-            for (var key in obj) {
-                if ({}.hasOwnProperty.call(obj, key)) {
-                    if (iterator.call(context, obj[key], key, obj) === breaker) return;
-                }
-            }
-        }
-    }
-
-    function map(obj, iterator, context) {
-        var results = [];
-        if (obj == null) return results;
-        if ([].map && obj.map === [].map) return obj.map(iterator, context);
-        each(obj, function (value, index, list) {
-            results[results.length] = iterator.call(context, value, index, list);
-        });
-        return results;
-    }
-
-    function cid(value) {
-        return value
-    }
-
-    return map(obj, cid);
-
-}
-
-console.log(getValues([ctx.__proto__]));
-
-// crossorigin image request
 function Texture(src){
 	this.image = new Image();
 	this.image.src = src;
@@ -102,6 +62,26 @@ function lines(){
 		linetest.draw();
 	});
 }
+
+const squares = JSON.stringify({
+	0 : ['rgba(250,0,0,0.5)',[10, 10, 50, 50]],
+	1 : ['rgba(0,250,0,0.5)',[20, 20, 50, 50]],
+	2 : ['rgba(0,0,250,0.5)',[30, 30, 50, 50]],
+	3 : ['rgba(250,0,0,0.5)',[40, 40, 50, 50]],
+	4 : ['rgba(0,250,0,0.5)',[50, 50, 50, 50]],
+	5 : ['rgba(0,0,250,0.5)',[60, 60, 50, 50]]
+});
+
+function filler(string, array){		
+	context.fillStyle = string
+	context.fillRect(array[0],array[1],array[2],array[3]);			
+}
+
+Object.values(JSON.parse(squares)).forEach(function(object){
+	filler( object[0], object[1] )
+});
+
+
 const grid = [
     1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 
     1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 
