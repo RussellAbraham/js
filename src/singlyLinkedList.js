a98b9ce8100404e4e202f47cddc63d7cf9e25524
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-10-31 18:19:29 -0700

    Update
    JavaScript Singly Linked List, 
    constructor, 
    prototype

--- src/singlyLinkedList.js
+++ src/singlyLinkedList.js
@@ -1,181 +1,148 @@
+
 "use strict";
 
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+function Node(value) {
+	this.value = value;
+	this.next = null;
+}
 
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+function SinglyLinkedList() {
+	this.head = null;
+	this.tail = null;
+	this.length = 0;
+}
 
-function _instanceof(left, right) { if (right != null && typeof Symbol !== "undefined" && right[Symbol.hasInstance]) { return !!right[Symbol.hasInstance](left); } else { return left instanceof right; } }
+SinglyLinkedList.prototype = {
+	constructor: SinglyLinkedList,
+	push: function (val) {
+		var newNode = new Node(val);
+		if (!this.head) {
+			this.head = newNode;
+			this.tail = this.head;
+		} else {
+			this.tail.next = newNode;
+			this.tail = newNode;
+		}
+		this.length++;
+		return this;
+	},
 
-function _classCallCheck(instance, Constructor) { if (!_instanceof(instance, Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+	pop: function () {
+		if (!this.head) return undefined;
+		var current = this.head;
+		var newTail = current;
 
-var Node = function Node(val) {
-  _classCallCheck(this, Node);
+		while (current.next) {
+			newTail = current;
+			current = current.next;
+		}
 
-  this.val = val;
-  this.next = null;
+		this.tail = newTail;
+		this.tail.next = null;
+		this.length--;
+
+		if (this.length === 0) {
+			this.head = null;
+			this.tail = null;
+		}
+
+		return current;
+	},
+
+	shift: function () {
+		if (!this.head) return undefined;
+		var currentHead = this.head;
+		this.head = currentHead.next;
+		this.length--;
+
+		if (this.length === 0) {
+			this.tail = null;
+		}
+
+		return currentHead;
+	},
+
+	unshift: function (val) {
+		var newNode = new Node(val);
+		if (!this.head) {
+			this.head = newNode;
+			this.tail = this.head;
+		}
+		newNode.next = this.head;
+		this.head = newNode;
+		this.length++;
+		return this;
+	},
+
+	get: function (index) {
+		if (index < 0 || index >= this.length) return null;
+		var counter = 0;
+		var current = this.head;
+		while (counter !== index) {
+			current = current.next;
+			counter++;
+		}
+		return current;
+	},
+
+	set: function (index, val) {
+		var foundNode = this.get(index);
+		if (foundNode) {
+			foundNode.value = val;
+			return true;
+		}
+		return false;
+	},
+
+	insert: function (index, val) {
+		if (index < 0 || index > this.length) return false;
+		if (index === this.length) return !!this.push(val);
+		if (index === 0) return !!this.unshift(val);
+		var newNode = new Node(val);
+		var prev = this.get(index - 1);
+		var temp = prev.next;
+		prev.next = newNode;
+		newNode.next = temp;
+		this.length++;
+		return true;
+	},
+
+	remove: function (index) {
+		if (index < 0 || index >= this.length) return undefined;
+		if (index === 0) return this.shift();
+		if (index === this.length - 1) return this.pop();
+		var previousNode = this.get(index - 1);
+		var removed = previousNode.next;
+		previousNode.next = removed.next;
+		this.length--;
+		return removed;
+	},
+
+	reverse: function () {
+		var node = this.head;
+		this.head = this.tail;
+		this.tail = node;
+		var next;
+		var prev = null;
+		for (var i = 0; i < this.length; i++) {
+			next = node.next;
+			node.next = prev;
+			prev = node;
+			node = next;
+		}
+		return this;
+	},
+
+	print: function () {
+		var arr = [];
+
+		var current = this.head;
+
+		while (current) {
+			arr.push(current.value);
+			current = current.next;
+		}
+
+		console.log(arr);
+	}
 };
-
-var SinglyLinkedList = /*#__PURE__*/function () {
-  function SinglyLinkedList() {
-    _classCallCheck(this, SinglyLinkedList);
-
-    this.head = null;
-    this.tail = null;
-    this.length = 0;
-  }
-
-  _createClass(SinglyLinkedList, [{
-    key: "push",
-    value: function push(val) {
-      var newNode = new Node(val);
-
-      if (!this.head) {
-        this.head = newNode;
-        this.tail = this.head;
-      } else {
-        this.tail.next = newNode;
-        this.tail = newNode;
-      }
-
-      this.length++;
-      return this;
-    }
-  }, {
-    key: "pop",
-    value: function pop() {
-      if (!this.head) return undefined;
-      var current = this.head;
-      var newTail = current;
-
-      while (current.next) {
-        newTail = current;
-        current = current.next;
-      }
-
-      this.tail = newTail;
-      this.tail.next = null;
-      this.length--;
-
-      if (this.length === 0) {
-        this.head = null;
-        this.tail = null;
-      }
-
-      return current;
-    }
-  }, {
-    key: "shift",
-    value: function shift() {
-      if (!this.head) return undefined;
-      var currentHead = this.head;
-      this.head = currentHead.next;
-      this.length--;
-
-      if (this.length === 0) {
-        this.tail = null;
-      }
-
-      return currentHead;
-    }
-  }, {
-    key: "unshift",
-    value: function unshift(val) {
-      var newNode = new Node(val);
-
-      if (!this.head) {
-        this.head = newNode;
-        this.tail = this.head;
-      }
-
-      newNode.next = this.head;
-      this.head = newNode;
-      this.length++;
-      return this;
-    }
-  }, {
-    key: "get",
-    value: function get(index) {
-      if (index < 0 || index >= this.length) return null;
-      var counter = 0;
-      var current = this.head;
-
-      while (counter !== index) {
-        current = current.next;
-        counter++;
-      }
-
-      return current;
-    }
-  }, {
-    key: "set",
-    value: function set(index, val) {
-      var foundNode = this.get(index);
-
-      if (foundNode) {
-        foundNode.val = val;
-        return true;
-      }
-
-      return false;
-    }
-  }, {
-    key: "insert",
-    value: function insert(index, val) {
-      if (index < 0 || index > this.length) return false;
-      if (index === this.length) return !!this.push(val);
-      if (index === 0) return !!this.unshift(val);
-      var newNode = new Node(val);
-      var prev = this.get(index - 1);
-      var temp = prev.next;
-      prev.next = newNode;
-      newNode.next = temp;
-      this.length++;
-      return true;
-    }
-  }, {
-    key: "remove",
-    value: function remove(index) {
-      if (index < 0 || index >= this.length) return undefined;
-      if (index === 0) return this.shift();
-      if (index === this.length - 1) return this.pop();
-      var previousNode = this.get(index - 1);
-      var removed = previousNode.next;
-      previousNode.next = removed.next;
-      this.length--;
-      return removed;
-    }
-  }, {
-    key: "reverse",
-    value: function reverse() {
-      var node = this.head;
-      this.head = this.tail;
-      this.tail = node;
-      var next;
-      var prev = null;
-
-      for (var i = 0; i < this.length; i++) {
-        next = node.next;
-        node.next = prev;
-        prev = node;
-        node = next;
-      }
-
-      return this;
-    }
-  }, {
-    key: "print",
-    value: function print() {
-      var arr = [];
-      var current = this.head;
-
-      while (current) {
-        arr.push(current.val);
-        current = current.next;
-      }
-
-      console.log(arr);
-    }
-  }]);
-
-  return SinglyLinkedList;
-}();
