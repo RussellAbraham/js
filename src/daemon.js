181a2fc59a8fab136812bef51bf721307090991d
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-11-02 15:07:28 -0800

    Update daemon.js
    Todo 
    * pause
    * rewind

--- src/daemon.js
+++ src/daemon.js
@@ -1,3 +1,30 @@
+
+/* mini daemon */
+
+const daemon = {
+    
+    start : function (ms) {
+        if (this.timerId) {
+            throw new Error(this.timerId);
+        }
+        else {
+            this.timerId = setInterval(function () {
+                // callback
+            }, ms);
+        }
+    },
+    
+    stop: function () {
+        if(this.timerId){
+            clearInterval(this.timerId);            
+            this.timerId = "";
+        }
+    }
+    
+}
+
+/* max daemon */
+
 function Daemon(context, callback, ms, steps, init, start){
 
     if(context){ this.context = context; }
@@ -34,155 +61,3 @@ Daemon.forceCall = function(object){
 Daemon.prototype.sync = function(){
     Daemon.forceCall(this);
 }
-
-/**/
-function Alerts(){
-    this.levels = [
-        "green", 
-        "blue", 
-        "yellow", 
-        "orange",
-        "red"
-    ];
-}
-
-Alerts.prototype = {
-    
-    randomLevel : function(){
-        const idx = Math.floor(Math.random() * this.levels.length);
-        return this.levels[idx];
-    },
-
-    start : function(ms){
-        this.cid = setInterval(function(){
-            console.log(this.randomLevel()) 
-        }, ms);
-    },
-    stop : function(){
-        clearInterval(this.cid);
-    }
-}
-
-const alerts = {
-
-    levels : [
-        "green", 
-        "blue", 
-        "yellow", 
-        "orange",
-        "red"
-    ],
-
-    indexLevel : function(){
-        const levels = alerts.levels.length;
-        const idx = Math.floor(Math.random() * levels);
-        return levels[idx];
-    },
-    start : function(ms){
-        
-        this.cid = setInterval(function(){
-            console.log(this.pickPhrase())        
-        }, ms);
-    },
-    stop() {
-        clearInterval(this.timerId);
-        console.log("PHEW THANK HEAVENS THAT IS OVER!")
-    }
-}
-
-const taskrunner0 = {
-    array: ['one', 'two', 'three'],
-    pick: function () {
-        const ids = Math.floor(Math.random() * this.array.length);
-        return this.array[ids]
-    },
-    start: function () {
-        var self = this;
-        this.timerId = setInterval(function () {
-
-            console.log(self.pick())
-
-        }, 1000);
-
-    },
-    stop: function () {
-        clearInterval(this.timerId);
-        console.log('taskrunner may have stopped');
-        // clear the timerId so it can start again
-        this.timerId = "";
-    }
-}
-
-const taskrunner = {
-    start: function () {
-        // if the timerId exists, dont start another one
-        if (this.timerId) {
-            console.log('already running')
-            return false
-        }
-        // if timerId does not exist, start the interval
-        else {
-            this.timerId = setInterval(function () {
-                // execute function
-            }, 1000);
-        }
-    },
-    stop: function () {
-        clearInterval(this.timerId);
-        console.log('taskrunner may have stopped');
-        // clear the timerId so it can start again
-        this.timerId = "";
-    }
-}
-// taskrunner.start();
-// taskrunner.stop();
-
-// todo: run more tests on separate threads
-const factoryWorker = function () {
-    return {
-        start: function () {
-            var root = this;
-            if (root.timerId) {
-                return false;
-            } else {
-                root.timerId = setInterval(function () {
-                    console.log('from factory worker')
-                }, 500)
-            }
-        },
-        stop: function () {
-            var root = this;
-            clearInterval(root.timerId);
-            console.log('factory worker may have stopped')
-        }
-    }
-
-}
-
-
-const FactoryWorker = function () {
-    this.isRunning = false;
-}
-
-FactoryWorker.prototype = {
-
-    start: function () {
-        var self = this;
-        if (this.isRunning) {
-            return false;
-        } else {
-            self.idx = new Worker('data:application/javascript, const myConst = this;');
-            this.isRunning = true;
-        }
-    },
-
-    stop: function () {
-        var self = this;
-        self.idx.terminate();
-        this.isRunning = false;
-    },
-
-}
-
-
-
