d5cc3a8f0d34101e5e0c64f65c61bd36c4362d89
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-11-23 20:04:29 -0800

    Update base.js
    Polyfill for ES6 class extends, preinitialization and initialization built in.
    
    Removed Emitter and DOM helpers

--- src/base.js
+++ src/base.js
@@ -1,54 +1,11 @@
 (function (global) {
-
-    window.qs = function (selector, scope) {
-        return (scope || document).querySelector(selector);
-    };
-
-    window.qsa = function (selector, scope) {
-        return (scope || document).querySelectorAll(selector);
-    };
-
-    window.$on = function (target, type, callback, useCapture) {
-        target.addEventListener(type, callback, !!useCapture);
-    };
-
-    window.$delegate = function (target, selector, type, handler) {
-        function dispatchEvent(event) {
-            var targetElement = event.target;
-            var potentialElements = window.qsa(selector, target);
-            var hasMatch =
-                Array.prototype.indexOf.call(potentialElements, targetElement) >= 0;
-            if (hasMatch) {
-                handler.call(targetElement, event);
-            }
-        }
-        var useCapture = type === "blur" || type === "focus";
-        window.$on(target, type, dispatchEvent, useCapture);
-    };
-
-    window.$parent = function (element, tagName) {
-        if (!element.parentNode) {
-            return;
-        }
-        if (element.parentNode.tagName.toLowerCase() === tagName.toLowerCase()) {
-            return element.parentNode;
-        }
-        return window.$parent(element.parentNode, tagName);
-    };
-
     function has(obj, key) {
         return obj != null && {}.hasOwnProperty.call(obj, key);
     }
-
     function isObject(obj) {
         var type = typeof obj;
         return type === "function" || (type === "object" && !!obj);
     }
-
-    function isFunction(obj) {
-        return !!(obj && obj.constructor && obj.call && obj.apply);
-    }
-
     function createAssigner(keysFunc, undefinedOnly) {
         return function (obj) {
             var length = arguments.length,
@@ -67,9 +24,7 @@
             return obj;
         };
     }
-
     const extendOwn = createAssigner(Object.keys);
-
     function extend(obj) {
         [].slice.call(arguments, 1).forEach(function (source) {
             for (var prop in source) {
@@ -78,9 +33,7 @@
         });
         return obj;
     }
-
-    function Ctor() {}
-
+    function Ctor() {};
     function baseCreate(prototype) {
         if (!isObject(prototype)) return {};
         if (Object.create) return Object.create(prototype);
@@ -89,13 +42,11 @@
         Ctor.prototype = null;
         return result;
     }
-
     function create(prototype, props) {
         var result = baseCreate(prototype);
         if (props) extendOwn(result, props);
         return result;
     }
-
     function inherits(protoProps, staticProps) {
         var parent = this;
         var child;
@@ -112,74 +63,11 @@
         child.__super__ = parent.prototype;
         return child;
     }
-
-    function Emitter() {}
-
-    Emitter.prototype = {
-        on: function (event, listener) {
-            this._eventCollection = this._eventCollection || {};
-            this._eventCollection[event] = this._eventCollection[event] || [];
-            this._eventCollection[event].push(listener);
-            return this;
-        },
-        once: function (event, listener) {
-            var self = this;
-
-            function fn() {
-                self.off(event, fn);
-                listener.apply(this, arguments);
-            }
-            fn.listener = listener;
-            this.on(event, fn);
-            return this;
-        },
-        off: function (event, listener) {
-            var listeners = undefined;
-            if (
-                !this._eventCollection ||
-                !(listeners = this._eventCollection[event])
-            ) {
-                return this;
-            }
-            listeners.forEach(function (fn, i) {
-                if (fn === listener || fn.listener === listener) {
-                    listeners.splice(i, 1);
-                }
-            });
-            if (listeners.length === 0) {
-                delete this._eventCollection[event];
-            }
-            return this;
-        },
-        emit: function (event) {
-            var _this = this,
-                _len = arguments.length;
-            for (
-                _len, args = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key += 1
-            ) {
-                args[_key - 1] = arguments[_key];
-            }
-            var listeners = undefined;
-            if (
-                !this._eventCollection ||
-                !(listeners = this._eventCollection[event])
-            ) {
-                return this;
-            }
-            listeners = listeners.slice(0);
-            listeners.forEach(function (fn) {
-                return fn.apply(_this, args);
-            });
-            return this;
-        }
-    };
-
-    function Base() {
+    global.Base = function(){
         this.preinitialize.apply(this, arguments);
         this.initialize.apply(this, arguments);
     }
-
-    Base.prototype = Object.create(Ctor.prototype, {
+    global.Base.prototype = Object.create(Ctor.prototype, {
         constructor: {
             configurable: true,
             enumerable: true,
@@ -187,42 +75,37 @@
             writable: true
         }
     });
-
-    extend(Base.prototype, new Emitter(), {
+    extend(global.Base.prototype, {
         preinitialize: function () {},
         initialize: function () {}
     });
 
-    Base.extend = inherits;
+    global.Base.extend = inherits;
 
-    if (typeof global !== "undefined") {
-        global.Base = Base;
-    }
 })(this);
 
 
 const Model = Base.extend({
-    /* 
-      Preinitialize and Initialize are overrideable functions 
-      but will always execute in the same order if either are defined 
-    */
-    initialize: function () {
-        console.log("Initialize Always Next");
-    },
-
     preinitialize: function () {
-        console.log("Preinitialize Always First");
+        console.log("1");
+    },
+    initialize: function () {
+        console.log("2");
     }
-
 });
 
 const View = Base.extend({
-
+    preinitialize: function () {
+        console.log(1);
+    },
     initialize: function () {
-        this.$body = qs(".markdown-body");
+        console.log(2);
     }
-
 });
 
-const model = new Model();
-const view = new View();
+function Main(){  
+	this.model = new Model();
+	this.view = new View();
+};
+
+const app = new Main();
