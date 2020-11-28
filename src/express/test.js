92615e4a57769221b8701cc55b588a3a773c6756
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-11-28 14:19:36 -0800

    Create test.js
    CTRL-C Signal to shut down nodejs express server.

---
+++ src/express/test.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+function action(object, events) {
+    for (var event in events) {
+        object.on(event, events[event]);
+    }
+}
+
+function pending(string) {
+    return new Promise(function (resolve, reject) {
+        if (string) {
+            resolve(string);
+        } else {
+            reject(string);
+        }
+    })
+}
+
+app.get('/', function(req, res){
+    console.log(this);
+    res.send('Hello World!');
+
+});
+
+const server = app.listen(port, function(){
+    console.log(`Express Server at http://localhost:${port}`, this);
+});
+
+
+function stop() {
+    
+    pending('signal')
+
+    .then(function(resolve){
+        console.info(resolve.concat(" received, closing"));
+    })
+
+    .then(function(){
+        server.close();
+        console.info('closed');        
+    })
+
+    .then(function(){
+        process.exit(0);
+    })
+
+    .catch(function(reject){
+        throw Error(reject.concat(' FAIL '))
+    })
+
+}
+
+action(process, {
+    'SIGINT': stop//,
+    //'SIGTERM': stop,
+    //'SIGHUP': stop
+});
