d2ec2cf83f32506a65c63ec1d5dda0b37314fbf9
Russell <35252806+RussellAbraham@users.noreply.github.com>
2021-01-21 02:40:17 -0800

    Update
    a mess of good object enumeration

--- src/eval.js
+++ src/eval.js
@@ -1,3 +1,7 @@
+// this file contains methods for inspecting javascripts standard objects and producing output using eval
+
+//
+
 this._ = (function (_) {
     
     function Constructor(name){	
@@ -149,6 +153,157 @@ var SimplePropertyRetriever = {
     }
 };
 
+
+function sortci(a, b) {
+    return a.toLowerCase() < b.toLowerCase() ? -1 : 1;
+  }
+  
+  // custom because I want to be able to introspect native browser objects *and* functions
+  function stringify(o, simple, visited) {
+    var json = '', i, vi, type = '', parts = [], names = [], circular = false;
+    visited = visited || [];
+  
+    try {
+      type = ({}).toString.call(o);
+    } catch (e) { // only happens when typeof is protected (...randomly)
+      type = '[object Object]';
+    }
+  
+    // check for circular references
+    for (vi = 0; vi < visited.length; vi++) {
+      if (o === visited[vi]) {
+        circular = true;
+        break;
+      }
+    }
+  
+    if (circular) {
+      json = '[circular]';
+    } else if (type == '[object String]') {
+      json = '"' + o.replace(/"/g, '\\"') + '"';
+    } else if (type == '[object Array]') {
+      visited.push(o);
+  
+      json = '[';
+      for (i = 0; i < o.length; i++) {
+        parts.push(stringify(o[i], simple, visited));
+      }
+      json += parts.join(', ') + ']';
+      json;
+    } else if (type == '[object Object]') {
+      visited.push(o);
+  
+      json = '{';
+      for (i in o) {
+        names.push(i);
+      }
+      names.sort(sortci);
+      for (i = 0; i < names.length; i++) {
+        parts.push( stringify(names[i], undefined, visited) + ': ' + stringify(o[ names[i] ], simple, visited) );
+      }
+      json += parts.join(', ') + '}';
+    } else if (type == '[object Number]') {
+      json = o+'';
+    } else if (type == '[object Boolean]') {
+      json = o ? 'true' : 'false';
+    } else if (type == '[object Function]') {
+      json = o.toString();
+    } else if (o === null) {
+      json = 'null';
+    } else if (o === undefined) {
+      json = 'undefined';
+    } else if (simple == undefined) {
+      visited.push(o);
+  
+      json = type + '{\n';
+      for (i in o) {
+        names.push(i);
+      }
+      names.sort(sortci);
+      for (i = 0; i < names.length; i++) {
+        try {
+          parts.push(names[i] + ': ' + stringify(o[names[i]], true, visited)); // safety from max stack
+        } catch (e) {
+          if (e.name == 'NS_ERROR_NOT_IMPLEMENTED') {
+            // do nothing - not sure it's useful to show this error when the variable is protected
+            // parts.push(names[i] + ': NS_ERROR_NOT_IMPLEMENTED');
+          }
+        }
+      }
+      json += parts.join(',\n') + '\n}';
+    } else {
+      try {
+        json = o+''; // should look like an object
+      } catch (e) {}
+    }
+    return json;
+  }
+
+ 
+  function toSource(func) { if (func != null) { try { return Function.prototype.toString.call(func); } catch (e) {} try { return func + ""; } catch (e) {} } return ""; }
+  function toProps(obj) { var resultSet = {}; for (var o = obj; o; o = o.__proto__) { try { var names = Object.getOwnPropertyNames(o); for (var i = 0; i < names.length; ++i) resultSet[names[i]] = "[" + typeof obj[names[i]] + "]"; } catch (e) {} } return JSON.stringify(resultSet, null, 2); }
+ 
+// if input has (
+
+    // -o
+    // > __proto__
+
+    // -p
+    // prototype
+
+    // -c
+    // constructor
+
+// )
+
+// run additional level of parsing if the indicated flags properties are present on the input, 
+
+// else return input value and error, that nested level, does not exist on the input
+
+
+function parse(string) {
+
+    var result;
+
+    if (typeof string == "function" || false) {
+        result = toSource(string);
+    }
+
+    else if (!!(string && string.test && string.exec && (string.ignoreCase || string.ignoreCase === false))) {
+        result = new RegExp(string).toString();
+    } 
+      
+    else if (!!(string && string.getTimezoneOffset && string.setUTCFullYear)) {
+        result = string.toString();
+    } 
+
+    else if (typeof string === "object" || false) {
+        result = toProps(string);
+    } 
+      
+    else if (!!(string === "" || (string && string.charCodeAt && string.substr))) {
+        result = string;
+    } 
+      
+    else if (!!(string === 0 || (string && string.toExponential && string.toFixed))) {          
+        result = string;
+    } 
+    
+    else if (string === true || string === false) {
+        result = string;
+    } 
+      
+    else if (string === void 0) {
+        result = undefined;
+    } 
+      
+    else if (string === null) {
+        result = null;
+    }
+
+    return result;
+
+}
 
 (function(global) {
     function toSource(func) { if (func != null) { try { return Function.prototype.toString.call(func); } catch (e) {} try { return func + ""; } catch (e) {} } return ""; }
