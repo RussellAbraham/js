195b64583b35898022aaecbf89f801d232d6612a
Russell <35252806+RussellAbraham@users.noreply.github.com>
2021-01-21 02:46:08 -0800

    create Event.js

---
+++ src/event.js
@@ -0,0 +1,56 @@
+// keyboard event
+
+document.addEventListener('keyup', function(event){
+    switch(event.which){
+        case 8 : console.log(event); break;
+        case 9 : console.log(event); break;
+        case 13 : console.log(event); break;
+        case 27 : console.log(event); break;
+        case 32 : console.log(event); break;
+        case 33 : console.log(event); break;
+        case 34 : console.log(event); break;
+        case 35 : console.log(event); break;
+        case 36 : console.log(event); break;
+        case 37 : console.log(event); break;
+        case 38 : console.log(event); break;
+        case 39 : console.log(event); break;
+        case 40 : console.log(event); break;
+        case 46 : console.log(event); break;
+        case 65 : console.log(event); break;
+        case 66 : console.log(event); break;
+        case 67 : console.log(event); break;
+        case 68 : console.log(event); break;
+        case 69 : console.log(event); break;
+        case 70 : console.log(event); break;
+        case 71 : console.log(event); break;
+        case 72 : console.log(event); break;
+        case 73 : console.log(event); break;
+        case 74 : console.log(event); break;
+        case 75 : console.log(event); break;
+        case 76 : console.log(event); break;
+        case 77 : console.log(event); break;
+        case 78 : console.log(event); break;
+        case 79 : console.log(event); break;
+        case 80 : console.log(event); break;
+        case 81 : console.log(event); break;
+        case 82 : console.log(event); break;
+        case 83 : console.log(event); break;
+        case 84 : console.log(event); break;
+        case 85 : console.log(event); break;
+        case 86 : console.log(event); break;
+        case 87 : console.log(event); break;
+        case 88 : console.log(event); break;
+        case 89 : console.log(event); break;
+        case 90 : console.log(event); break;
+        case 57 : console.log(event); break;
+        case 56 : console.log(event); break;
+        case 55 : console.log(event); break;
+        case 54 : console.log(event); break;
+        case 53 : console.log(event); break;
+        case 52 : console.log(event); break;
+        case 51 : console.log(event); break;
+        case 50 : console.log(event); break;
+        case 4 :  console.log(event);break;
+        default : console.log(event); break;                
+    }
+}, false);
