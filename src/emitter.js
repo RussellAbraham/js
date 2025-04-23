6ca95f87ad20f3dd818dd6ebcfb2317a2e5a2a70
Russell <35252806+RussellAbraham@users.noreply.github.com>
2025-04-22 22:54:16 -0700

    Update emitter.js
    Added new version of the EventEmitter class

--- src/emitter.js
+++ src/emitter.js
@@ -131,3 +131,37 @@ Emitter.prototype.emit = function(event){
     });    
 };
 
+
+// EventEmitter.js
+
+export default class EventEmitter {
+  constructor() {
+    this.events = {};
+  }
+
+  on(event, listener) {
+    (this.events[event] ||= []).push(listener);
+    return this;
+  }
+
+  off(event, listener) {
+    const listeners = this.events[event];
+    if (!listeners) return this;
+
+    this.events[event] = listeners.filter(fn => fn !== listener && fn.listener !== listener);
+    if (this.events[event].length === 0) {
+      delete this.events[event];
+    }
+
+    return this;
+  }
+
+  emit(event, ...args) {
+    const listeners = this.events[event];
+    if (!listeners) return this;
+
+    listeners.forEach(fn => fn.apply(this, args));
+    return this;
+  }
+}
+
